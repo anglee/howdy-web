bc3d4fc0a7fda3024f88b6ac1d0104fa4cca56fa
anglee <ang.lee@gmail.com>
2016-03-12 14:09:47 -0500

    Brute force

--- howdy.js
+++ howdy.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (string) => {
+  let ret = '';
+  for (let i = 0; i < string.length; i++) {
+    const set = new Set();
+    let j = i;
+    for (; j < string.length; j++) {
+      if (set.has(string[j])) {
+        break;
+      } else {
+        set.add(string[j]);
+      }
+    }
+    if (j - i > ret.length) {
+      ret = string.substring(i, j);
+    }
+  }
+  return ret;
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,25 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('find the longest substring without repeating characters', () => {
+    const input = 'abcabcbb';
+    expect(howdy(input)).to.equal('abc');
   });
+  it('find the longest substring without repeating characters', () => {
+    const input = 'bcbcabc';
+    expect(howdy(input)).to.equal('bca');
+  });
+  it('find the longest substring without repeating characters', () => {
+    const input = 'bcbcabcd';
+    expect(howdy(input)).to.equal('abcd');
+  });
+  it('find the longest substring without repeating characters', () => {
+    const input = 'bbbbb';
+    expect(howdy(input)).to.equal('b');
+  });
+  it('find the longest substring without repeating characters', () => {
+    const input = '';
+    expect(howdy(input)).to.equal('');
+  });
+
 });
