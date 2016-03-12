57097fd8005207a69a52f01369ca04cbaf460863
anglee <ang.lee@gmail.com>
2016-03-12 11:51:07 -0500

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,18 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (array, sum) => {
+  const map = new Map();
+
+  _.forEach(array, (it, j) => {
+    map.set(it, j);
+  });
+
+  for (let i = 0; i < array.length; i++) {
+    const j = map.get(sum - array[i]);
+    if (!_.isUndefined(j)) {
+      return [i, j];
+    }
+  }
+};
 
 export default howdy;
