2639f0472f2d2d2433ccda61fd3ac0b9916b4e01
anglee <ang.lee@gmail.com>
2016-03-12 11:27:43 -0500

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,13 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (array, sum) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === sum) {
+        return [i, j];
+      }
+    }
+  }
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,18 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should find the 2 elements(indices) that add up to the sum given the array and the sum', () => {
+    const array = [1,2,4,8,16];
+    const sum = 6;
+    const [index1, index2] = howdy(array, sum);
+    expect(index1).to.equal(1);
+    expect(index2).to.equal(2);
+  });
+  it('should find the 2 elements(indices) that add up to the sum given the array and the sum', () => {
+    const array = [1, 2, 4, 8, 16];
+    const sum = 10;
+    const [index1, index2] = howdy(array, sum);
+    expect(index1).to.equal(1);
+    expect(index2).to.equal(3);
   });
 });
