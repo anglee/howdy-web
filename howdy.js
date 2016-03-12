1b797c7cc066372443e46512517431cac57ba152
anglee <ang.lee@gmail.com>
2016-03-12 13:44:53 -0500

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
-
-export default howdy;
+export const isPalindrome = ((input) => {
+  const processedInput = _.words(input).join('').toUpperCase();
+  let i = 0, j = processedInput.length - 1;
+  while (i < j) {
+    if (processedInput[i] === processedInput[j]) {
+      i++;
+      j--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+});
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,21 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import {isPalindrome} from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should be able to determine the input string is a palindrome', () => {
+    const input = 'A man, a plan, a canal: Panama';
+    expect(isPalindrome(input)).to.be.true;
+  });
+  it('should be able to determine the input string is a palindrome', () => {
+    const input = 'race a car';
+    expect(isPalindrome(input)).to.be.false;
+  });
+  it('should be able to determine the input string is a palindrome', () => {
+    const input = 'ABA';
+    expect(isPalindrome(input)).to.be.true;
+  });
+  it('should be able to determine the input string is a palindrome', () => {
+    const input = 'ABC';
+    expect(isPalindrome(input)).to.be.false;
   });
 });
