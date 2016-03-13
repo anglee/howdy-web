dfbbb44765ade62ad7b675dab50aa18219772d4e
anglee <ang.lee@gmail.com>
2016-03-13 16:48:49 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,16 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (sortedInts) => {
+  const A = [-1, ...sortedInts, 100];
+  const ret = [];
+  for (let i = 0; i < A.length - 1; i++) {
+    if (A[i+1] - A[i] === 2) {
+      ret.push(`${A[i] + 1}`);
+    } else if (A[i+1] - A[i] > 2) {
+      ret.push(`${A[i] + 1}->${A[i+1] - 1}`);
+    }
+  }
+  return ret;
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,23 @@
+import _ from 'lodash';
 import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('given sorted integer array in range [0,99] inclusive, return its missing ranges.', () => {
+    it('given example', () => {
+      const given = [0, 1, 3, 50, 75];
+      const expected = ["2", "4->49", "51->74", "76->99"];
+      expect(howdy(given)).to.eql(expected);
+    });
+    it('empty input', () => {
+      const given = [];
+      const expected = ["0->99"];
+      expect(howdy(given)).to.eql(expected);
+    });
+    it('empty output.', () => {
+      const given = _.range(0, 100);
+      const expected = [];
+      expect(howdy(given)).to.eql(expected);
+    });
   });
 });
