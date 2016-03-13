8bafd4590269a62ee9adec476412c86964130941
anglee <ang.lee@gmail.com>
2016-03-13 17:09:20 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,16 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should find the longest palindromic substring', () => {
+    const input = 'acbcd';
+    expect(howdy(input)).to.equal('cbc');
+  });
+  it('should find the longest palindromic substring', () => {
+    const input = 'abc';
+    expect(howdy(input)).to.equal('a');
+  });
+  it('should find the longest palindromic substring', () => {
+    const input = 'a';
+    expect(howdy(input)).to.equal('a');
   });
 });
