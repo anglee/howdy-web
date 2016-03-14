0bd4d601dea50ed1034e78e42e079aa58ac9a21f
anglee <ang.lee@gmail.com>
2016-03-13 21:22:07 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,22 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('should reverse Integer', () => {
+    it('should reverse 0', () => {
+      expect(howdy(0)).to.equal(0);
+    });
+    it('should reverse 1', () => {
+      expect(howdy(1)).to.equal(1);
+    });
+    it('should reverse 123', () => {
+      expect(howdy(123)).to.equal(321);
+    });
+    it('should reverse 120', () => {
+      expect(howdy(120)).to.equal(21);
+    });
+    it('should reverse 1234', () => {
+      expect(howdy(1234)).to.equal(4321);
+    });
   });
+
 });
