b631f242d7eba47b11eeb40a5ed0c3b9a7af60f1
anglee <ang.lee@gmail.com>
2016-03-13 22:35:58 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,17 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const longer = (a, b) => a.length > b.length ? a : b;
+const howdy = (A) => {
+  const LISs = [];
+  for (let i = 0; i < A.length; i++) {
+    LISs.push([A[i]]);
+    for (let j = 0; j < i; j++) {
+      if (A[j] < A[i]) {
+        LISs[i] = longer(LISs[i], [...LISs[j], A[i]]);
+      }
+    }
+  }
+  return _.isEmpty(LISs) ? [] : _.maxBy(LISs, it => it.length);
+};
 
 export default howdy;
