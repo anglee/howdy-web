0b52c369b4c8f41a901d0ca42c4eaf27fee75b69
anglee <ang.lee@gmail.com>
2016-03-14 21:14:13 -0400

    LCS solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,21 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('should find howdy of S1 and S2', () => {
+    it('example1', () => {
+      const S1 = 'ABXC';
+      const S2 = 'AWBC';
+      expect(howdy(S1, S2)).to.equal('ABC');
+    });
+    it('example1', () => {
+      const S1 = 'ABCDGH';
+      const S2 = 'AEDFHR';
+      expect(howdy(S1, S2)).to.equal('ADH');
+    });
+    it('example1', () => {
+      const S1 = 'AGGTAB';
+      const S2 = 'GXTXAYB';
+      expect(howdy(S1, S2)).to.equal('GTAB');
+    });
   });
 });
