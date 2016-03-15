29dca56360777191de38a9078065fb87f3d216bc
anglee <ang.lee@gmail.com>
2016-03-14 22:04:15 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,28 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (starts, ends) => {
+  const pts = [
+    ...starts.map(p => ({at: p, start: true})),
+    ...ends.map(p => ({at: p, start: false}))
+  ];
+
+  let layer = 0;
+  let overlapStart;
+  let totalOverlap = 0;
+  _.sortBy(pts, 'at').forEach((pt) => {
+    if (pt.start) {
+      layer++;
+      if (layer === 2) {
+        overlapStart = pt.at;
+      }
+    } else {
+      layer--;
+      if (layer === 1) {
+        totalOverlap += pt.at - overlapStart;
+      }
+    }
+  });
+  return totalOverlap;
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,16 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import fraudFeet from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('should find total overlap', () => {
+    it('example 1', () => {
+      expect(fraudFeet([50, 50, 50], [58, 58, 58])).to.equal(8);
+    });
+    it('example 2', () => {
+      expect(fraudFeet([171234, 12, 20, 30], [171236, 20, 30, 40])).to.equal(0);
+    });
+    it('example 3', () => {
+      expect(fraudFeet([12, 32, 92], [991, 161, 1093])).to.equal(959);
+    });
   });
 });
