bd959159223dbcbbf0f4e8da39904ce0e0568db5
anglee <ang.lee@gmail.com>
2016-03-14 21:43:35 -0400

    DFS, BFS solved

---
+++ Node.js
@@ -0,0 +1,9 @@
+class Node {
+  constructor(val, leftChild = null, rightChild = null) {
+    this.val = val;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+}
+
+export default Node;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,27 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import {BFS, DFS} from './howdy';
+import Node from './Node';
+
+const A = new Node('A');
+const B = new Node('B');
+const C = new Node('C', A, B);
+const D = new Node('D');
+const E = new Node('E');
+const F = new Node('F', D, E);
+const G = new Node('G', C, F);
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('BFS', () => {
+    it('should traverse the tree', () => {
+      expect(BFS(G)).to.equal('GCABFDE');
+    });
+  });
+
+  describe('DFS', () => {
+    it('should traverse the tree', () => {
+      expect(DFS(G)).to.equal('GCFABDE');
+    });
   });
 });
+
+
