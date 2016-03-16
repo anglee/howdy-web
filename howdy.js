c188a701a616def5c712df0612e66df979de22ae
anglee <ang.lee@gmail.com>
2016-03-16 06:49:08 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,35 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+
+const move = (AA, x, w) => {
+  const y = x;
+  if (w === 1) {
+
+    return AA[x][y];
+  }
+  let top = '';
+  let right = '';
+  let bottom = '';
+  let left = '';
+  for (let i = 0; i < w - 1; i++) {
+    top += AA[y][x + i]; // start at (x,y) and move right
+    bottom += AA[y + w - 1][x + w - 1 - i]; // start at (x+w-1,y+w-1) and move left
+    right += AA[y + i][x + w - 1]; // start at (x+w-1,y) and move down
+    left += AA[y + w - 1 - i][x]; // start at (x,y+w-1) and move up
+  }
+  return top + right + bottom + left;
+};
+
+const howdy = (AA) => {
+  //return [1, 2, 3, 6, 9, 8, 7, 4, 5];
+  const w = AA.length;
+  let x = 0;
+  let ret = '';
+  for (let iw = w; iw > 0; iw -= 2) {
+    ret += move(AA, x, iw);
+    x++;
+  }
+  return ret.split('').map(it => parseInt(it));
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,22 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should traverse a squre in viral order', () => {
+    expect(howdy([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ])).to.eql([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+  });
+  it('should traverse a squre in viral order', () => {
+    expect(howdy([
+      [1]
+    ])).to.eql([1]);
+  });
+  it('should traverse a squre in viral order', () => {
+    expect(howdy([
+      [1, 2],
+      [3, 4]
+    ])).to.eql([1, 2, 4, 3]);
   });
 });
