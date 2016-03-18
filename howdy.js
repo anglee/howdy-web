8d6e4f79bbe0885ec91bbc29760e3b21cc1f596d
anglee <ang.lee@gmail.com>
2016-03-17 22:02:23 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,16 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const minPalin = (S) => {
+  if (S.length < 2) { return 0; }
 
-export default howdy;
+  if (S[0] === _.last(S)) {
+    return minPalin(S.substring(1, S.length - 1));
+  } else {
+    return Math.min(minPalin(S.substring(1)),
+        minPalin(S.substring(0, S.length - 1))) + 1;
+  }
+};
+
+// http://www.geeksforgeeks.org/dynamic-programming-set-28-minimum-insertions-to-form-a-palindrome/
+
+export default minPalin;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,50 @@
 import {expect} from 'chai';
 import howdy from './howdy';
+// A palindrome is a string which reads the same forward and backwards.
+// You are provided a function shortPalin which accepts a string S.
+// You are required to form a palindrome by adding 0 or more characters to the string at any
+// location which you find appropriate. Return the minimum number of characters which needs to be
+// added to the string to transform it into a palindrome. The section of the program which parses
+// the input and displays the output is already written. Your task is to complete the body of the
+// function provided, and to return the correct output.
 
 describe('howdy', () => {
   it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+    const input = "abab";
+    const expected = 1;
+    expect(howdy(input)).to.equal(expected);
+    // A palindrome can be obtained by appending an 'a' to the end of the string to form 'ababa'.
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "abacaba";
+    const expected = 0;
+    expect(howdy(input)).to.equal(expected);
+    // The string is already a palindrome, so no characters need to be added to it
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "ab";
+    const expected = 1;
+    expect(howdy(input)).to.equal(expected);
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "aa";
+    const expected = 0;
+    expect(howdy(input)).to.equal(expected);
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "abcd";
+    const expected = 3;
+    expect(howdy(input)).to.equal(expected);
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "abcda";
+    const expected = 2;
+    expect(howdy(input)).to.equal(expected);
+    // is the same as input = 'bcd', why?
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = "abcde";
+    const expected = 4;
+    expect(howdy(input)).to.equal(expected);
   });
 });
