295251bc19b10f423774cf2ddfdfd9f21c275018
anglee <ang.lee@gmail.com>
2016-03-20 16:07:30 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (S) => {
+  if (_.isEmpty(S)) {
+    return '';
+  }
+  let last = S[0];
+  let count = 1;
+  let result = '';
+  for (let i = 1; i < S.length; i++) {
+    if (S[i] !== last) {
+      result += last + count;
+      count = 0;
+    }
+    last = S[i];
+    count++;
+  }
+  result += last + count;
+  return result.length > S.length ? S : result;
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,16 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should compress strings', () => {
+    const input = "aabcccccaaa";
+    const expected = "a2b1c5a3";
+    expect(howdy(input)).to.equal(expected);
+  });
+  it('should should return the original string if ther compressed result is not shorter', () => {
+    const input = "abc";
+    const wrongResult = "a1b1c1";
+    const expected = "abc";
+    expect(howdy(input)).to.equal(expected);
+    expect(howdy(input)).to.not.equal(wrongResult);
   });
 });
