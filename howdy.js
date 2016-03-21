a92b6c6e0addf7131d5142ad2f77141c85a4f5ef
anglee <ang.lee@gmail.com>
2016-03-21 06:12:47 -0400

    Implemented, deleteNode and reverseLinkedList

--- howdy.js
+++ howdy.js
@@ -1,5 +1,47 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-export default howdy;
+const linkedListToString = (node) => {
+  let ret = '';
+  while (node) {
+    ret += ret === '' ? node.data : ", " + node.data
+    node = node.next;
+  }
+  return ret;
+};
+
+const deleteNode = (head, index) => {
+  if (index === 0) {
+    return head.next;
+  }
+  let node = head;
+  let last = null;
+  let i = 0;
+  while (i < index) {
+    last = node;
+    node = node.next;
+    i++;
+  }
+  last.next = node.next;
+
+  return head;
+};
+
+const reverseLinkedList = (head) => {
+  let p = head, pprev = null;
+  while (p) {
+    let pnext = p.next;
+    p.next = pprev;
+    pprev = p;
+    p = pnext;
+  }
+  return pprev;
+};
+
+export {Node, linkedListToString, deleteNode, reverseLinkedList};
