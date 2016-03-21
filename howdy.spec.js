bbd6d7cd65c31b3e472f58ba772c810b55811432
anglee <ang.lee@gmail.com>
2016-03-21 07:00:35 -0400

    Examples of to.be.equal and to.not.equal

--- howdy.spec.js
+++ howdy.spec.js
@@ -3,6 +3,9 @@ import howdy from './howdy';
 
 describe('howdy', () => {
   it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+    const A = [{foo: "bar"}, {goo: "mar"}];
+    const B = [{foo: "bar"}, {goo: "mar"}];
+    expect(A).to.not.equal(B);
+    expect(A).to.deep.equal(B);
   });
 });
