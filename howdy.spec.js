74f06728a5d2d50015d4eca94397c0f83ec61518
anglee <ang.lee@gmail.com>
2016-03-22 08:07:19 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,52 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import test from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('test', () => {
+  describe("should check regular express with '.' and '*'", () => {
+    it('base cases', () => {
+      expect(test('', '')).to.be.true;
+      expect(test('a', '')).to.be.false;
+    });
+
+    it("when pattern doesn't start with star", () => {
+      expect(test('a', 'a')).to.be.true;
+      expect(test('a', '.')).to.be.true;
+    });
+
+    it("when pattern starts with star", () => {
+      expect(test('', 'a*')).to.be.true;
+      expect(test('a', 'a*')).to.be.true;
+      expect(test('aa', 'a*')).to.be.true;
+      expect(test('', '.*')).to.be.true;
+      expect(test('a', '.*')).to.be.true;
+      expect(test('aa', '.*')).to.be.true;
+    });
+
+    it('test star followed by non-empty (not immediate into base cases)', () => {
+      expect(test('b', 'a*b')).to.be.true;
+      expect(test('b', 'a*c')).to.be.false;
+    });
+
+    it('test consecutive repeat of charactor, where $* should be ignored', () => {
+      expect(test('ab', 'a*ab')).to.be.true;
+      expect(test('ab', '.*ab')).to.be.true;
+      expect(test('ab', 'a*.b')).to.be.true;
+
+      expect(test('ab', 'aa*b')).to.be.true;
+      expect(test('ab', '.a*b')).to.be.true;
+      expect(test('ab', 'a.*b')).to.be.true;
+    });
+
+    it('a couple random tests', () => {
+      expect(test('abc', 'abc')).to.be.true;
+      expect(test('aaa', 'abc')).to.be.false;
+      expect(test('abc', 'a.c')).to.be.true;
+      expect(test('ab', 'a*b')).to.be.true;
+      expect(test('aab', 'a*b')).to.be.true;
+      expect(test('aaac', 'a.*c')).to.be.true;
+      expect(test('aaac', 'b*a.*c')).to.be.true;
+    });
   });
 });
+
+
