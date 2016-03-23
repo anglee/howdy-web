47690b3531f32ca679bd608818b6b26fd6eef6e0
anglee <ang.lee@gmail.com>
2016-03-23 08:27:54 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const cd = (pwd, path) => {
+  const stack = pwd.split('/');
+  path.split('/').forEach((dir) => {
+    if (dir === '..') {
+      stack.pop();
+    } else {
+      stack.push(dir);
+    }
+  });
+  return stack.join('/');
+};
 
-export default howdy;
+export default cd;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,16 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import cd from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('cd', () => {
+  describe('should change director' +
+    'given current director(pwd) and the relative path', () => {
+
+    it('should work', () => {
+      expect(cd('/github/anglee', 'foo/bar')).to.equal('/github/anglee/foo/bar');
+    });
+
+    it('should work with ..', () => {
+      expect(cd('/github/anglee', '../foo/bar')).to.equal('/github/foo/bar');
+    });
   });
 });
