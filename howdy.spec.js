2c882e369a79666ff4572d19825a8f9fcee37923
anglee <ang.lee@gmail.com>
2016-03-23 07:36:22 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,33 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  describe('should find merged segments', () => {
+
+    it('should work on the given example', () => {
+      const input = [[1,5],[10,15],[5,8],[17,20],[13,18]];
+      const output = [[1,8],[10,20]];
+      expect(howdy(input)).to.eql(output);
+    });
+
+    it('should work in the non-overlapping case', () => {
+      const input = [[1,5],[10,15]];
+      const output = [[1,5],[10,15]];
+      expect(howdy(input)).to.eql(output);
+    });
+    it('should work in the overlapping case', () => {
+      const input = [[1,5],[2,7]];
+      const output = [[1,7]];
+      expect(howdy(input)).to.eql(output);
+    });
+    it('should work when one completely contains another', () => {
+      const input = [[1,15],[5,10]];
+      const output = [[1,15]];
+      expect(howdy(input)).to.eql(output);
+    });
+    it('should work on empty input', () => {
+      const input = [];
+      const output = [];
+      expect(howdy(input)).to.eql(output);
+    });
   });
 });
