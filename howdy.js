9ac6f9df855db311da5799d291f99592e4fcbfad
anglee <ang.lee@gmail.com>
2016-03-24 09:08:12 -0400

    Not solved with the correct solution yet

--- howdy.js
+++ howdy.js
@@ -1,5 +1,62 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const bruteForceSolution = (A) => {
+  let minAvg = Number.POSITIVE_INFINITY;
+  let minAvgI = null;
 
-export default howdy;
+  for (let i = 0; i < A.length - 1; i++) {
+    for (let j = i + 1; j < A.length; j++) {
+      let sum = 0;
+      for (let k = i; k <= j; k++) {
+        sum += A[k];
+      }
+      const avg = sum / (j - i + 1);
+      if (avg < minAvg) {
+        minAvg = avg;
+        minAvgI = i;
+      }
+    }
+  }
+  return minAvgI;
+};
+
+const solution = (A) => {
+  const minAvgEndWithJ = _.range(A.length).map(()=>Number.POSITIVE_INFINITY);
+  const theIAtJWhenMinAvg = _.range(A.length).map(() => null);
+  let i = 0;
+  let sum = A[0];
+
+  for (let j = 1; j < A.length; j++) {
+    sum += A[j];
+
+    while (i < j - 1) {
+      const avgWithAi = sum / (j - i + 1);
+      const sumWithoutAi = sum - A[i];
+      const avgWithoutAi = sumWithoutAi / (j - i);
+      if (avgWithoutAi < avgWithAi) {
+        // drop Ai
+        sum -= A[i];
+        ++i;
+      } else {
+        break;
+      }
+    }
+    minAvgEndWithJ[j] = sum / (j - i + 1);
+    theIAtJWhenMinAvg[j] = i;
+  }
+
+  //console.log('minAvgEndWithJ', minAvgEndWithJ);
+  //console.log('theIAtJWhenMinAvg', theIAtJWhenMinAvg);
+
+  let minAvg = Number.POSITIVE_INFINITY;
+  let ret = null;
+  for (let j = 1; j < A.length; j++) {
+    if (minAvgEndWithJ[j] < minAvg) {
+      minAvg = minAvgEndWithJ[j];
+      ret = theIAtJWhenMinAvg[j];
+    }
+  }
+  return ret;
+};
+
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,62 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('solution', () => {
+  it('should work', () => {
+    const A = [4, 2, 2, 5, 1, 5, 8];
+    expect(solution(A)).to.equal(1);
+  });
+
+  it('should work', () => {
+    const A = [1,2,3];
+    expect(solution(A)).to.equal(0);
+  });
+
+  it('should work', () => {
+    const A = [3,1,2];
+    expect(solution(A)).to.equal(1);
+  });
+
+  it('should work', () => {
+    const A = [3,2,1];
+    expect(solution(A)).to.equal(1);
+  });
+
+  it('should work', () => {
+    const A = [2,5,1];
+    expect(solution(A)).to.equal(0);
+  });
+
+  it('should work', () => {
+    const A = [7, 1, 6];
+    expect(solution(A)).to.equal(1);
+  });
+
+  it('should work', () => {
+    const A = [7, 1, 8];
+    expect(solution(A)).to.equal(0);
+  });
+
+  it('should work', () => {
+    const A = [2, 2, 2];
+    expect(solution(A)).to.equal(0);
+  });
+
+  it('should work', () => {
+    const A = [2, 2, 3];
+    expect(solution(A)).to.equal(0);
+  });
+
+  it('should work', () => {
+    const A = [2, 2, 1];
+    expect(solution(A)).to.equal(1);
   });
 });
+
+
+// https://codesays.com/2014/solution-to-min-avg-two-slice-by-codility/
+//The key to solve this task is these two patterns:
+//  (1) There must be some slices, with length of two or three, having the
+//      minimal average value among all the slices.
+//  (2) And all the longer slices with minimal average are built up with
+//      these 2-element and/or 3-element small slices.
