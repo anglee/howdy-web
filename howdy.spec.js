781c3f9b375a526de795933cc6078492ac4e97e4
anglee <ang.lee@gmail.com>
2016-03-24 07:14:06 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,11 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('solution', () => {
+  it('should work', () => {
+    const A = [3,4,4,6,1,4,4];
+    const N = 5;
+    const result = [3,2,2,4,2];
+    expect(solution(N, A)).to.eql(result);
   });
 });
