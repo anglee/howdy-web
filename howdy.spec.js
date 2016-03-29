0f5e9fbdc78a2279ad3a4ff33ef9920e5afa7103
anglee <ang.lee@gmail.com>
2016-03-29 08:38:06 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,15 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import {bruteForceSolution, solution} from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('bruteForceSolution', () => {
+  it('should find how many triangles can be formed, given sorted A', () => {
+    const A = [2,3,4,5];
+    // 3 triangles can be formed with edges selected from A:
+    // [2,3,4], [2,4,5], [3,4,5]
+    expect(bruteForceSolution(A)).to.equal(3);
   });
 });
+
+// the instruction in the pdf was weird, my solution is essentially the same as
+// the given solution, which it claims to be O(n^2), but I think it should be O(n^3) instead
+// not sure if there is a O(n^2) solution.
