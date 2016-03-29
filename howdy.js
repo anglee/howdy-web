12dd1e3e7697d25f31caf012cf65855293a00a66
anglee <ang.lee@gmail.com>
2016-03-29 09:16:37 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const binarySearchI = (A, i, j, target) => {
+  if (j < i) {
+    return -1;
+  }
+  if (i === j) {
+    return A[i] > target ? i : -1;
+  }
+  const m = Math.floor((i + j) / 2);
+  if (A[m] > target) {
+    return binarySearchI(A, i, m, target);
+  } else { // A[m] <= target
+    return binarySearchI(A, m + 1, j, target);
+  }
+};
 
-export default howdy;
+const binarySearch = (A, target) => {
+  return binarySearchI(A, 0, A.length - 1, target);
+};
+
+export default binarySearch;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,24 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import binarySearch from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('binarySearch', () => {
+  it('find the index of the first element that is greater than target', () => {
+    const A = [1,2,3,4,6];
+    const target = 3;
+    // 4 is the first element in A that A[i] > 3
+    expect(binarySearch(A, target)).to.equal(A.indexOf(4));
+  });
+
+  describe('should return -1 when there is no element in A > target', () => {
+    it('should work with non-empty A', () => {
+      const A = [1,2,3,4];
+      const target = 5;
+      expect(binarySearch(A, target)).to.equal(-1);
+    });
+    it('should work with empty A', () => {
+      const A = [];
+      const target = 5;
+      expect(binarySearch(A, target)).to.equal(-1);
+    });
   });
 });
