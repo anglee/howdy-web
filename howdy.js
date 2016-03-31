29d391025300154699c5f40d9d611d510e303c64
anglee <ang.lee@gmail.com>
2016-03-31 12:08:13 -0400

    Update solution

--- howdy.js
+++ howdy.js
@@ -11,38 +11,48 @@ const solution = (A) => {
   // to form triangle, these 3 conditions needs to be true:
   // cond1: A[i] < A[j] + A[k]
   // cond2: A[j] < A[i] + A[k] --> A[i] > A[j] - A[k]
-  // cond3: A[k] < A[i] + A[j] --> A[i] > A[k] - A[j]
+  // cond3: A[k] < A[i] + A[j]
   //
-  // since A is sorted and we know j < k, thus A[j] must < A[k], so A[j] - A[k] is negative,
-  // and A[i] is positive, so cond2 is always true automatically, we don't need to care about it
-  // we only need to make sure cond1 and cond3 are true when we count the triangle (i,j,k)
-  // so we need to check:
-  // A[i] < A[j] + A[k]
-  // A[i] > A[k] - A[j]
+  // since A is sorted and we know i < j, A[i] must < A[j] and thus A[i] < A[j] + A[k] is always true
+  // so we don't need to check cond1
+  //
+  // since A is sorted and we know j < k, thus A[j] must < A[k], so A[j] - A[k] must be negative,
+  // and A[i] is positive,and thus A[i] > A[j] - A[k] is always true
+  // so we don't need to check cond2
+  //
+  // so, we only need to make sure cond3 is true when we count the triangle (i,j,k)
+  // that is, A[k] < A[i] + A[j]
 
   // for each possible i, count the triangles
   for (let i of _.range(A.length - 2)) {
     let j = i + 1;
     let k = i + 2;
 
-    while (k < A.length) {
-      if (A[i] < A[j] + A[k] && A[i] > A[k] - A[j]) {
-        count++;
-        console.log(`triangle (${A[i]}, ${A[j]}, ${A[k]})`);
-      }
+    while (j < A.length - 1) {
 
-      // either move j or k
-      if (A[i] + A[j] <= A[k] || k === A.length - 1) { // move j if A[i] + A[j] <= A[k] or k cannot be moved
-        j++;
-        // after j is moved, make sure k is still > j
-        if (j === k) {
-          k++;
+      // push k to the left as much as possible
+      while (A[k] < A[i] + A[j] && k < A.length) {
+        console.log(`triangle (${A[i]}, ${A[j]}, ${A[k]})`);
+        count++;
+
+        if (k === A.length - 1) { // if k is already at the end, we are done
+          break;
         }
-      } else {
+
         k++;
       }
+
+      // at this point we've counted all triangles that use i and j in the while () { k++ } loop above
+      // and we are about to move to the next j (as shown as j++ below)
+      // note that, here, in a block of each i, k will never move left ever (and thus it's O(n^2) and not O(n^3)
+
+      j++;
+      // make sure j > k
+      k = Math.max(k, j + 1);
     }
   }
+
+
   return count;
 };
 
