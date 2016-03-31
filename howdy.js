6dba5c9bb5e9f2d1809c1fc0921c039344e7ac68
anglee <ang.lee@gmail.com>
2016-03-31 11:13:33 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,49 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const solution = (A) => {
 
-export default howdy;
+  // sort A first
+  A = _.sortBy(A);
+
+  let count = 0;
+
+  // use 3 pointers, i, j, k and ensure it so that i < j < k
+  // to form triangle, these 3 conditions needs to be true:
+  // cond1: A[i] < A[j] + A[k]
+  // cond2: A[j] < A[i] + A[k] --> A[i] > A[j] - A[k]
+  // cond3: A[k] < A[i] + A[j] --> A[i] > A[k] - A[j]
+  //
+  // since A is sorted and we know j < k, thus A[j] must < A[k], so A[j] - A[k] is negative,
+  // and A[i] is positive, so cond2 is always true automatically, we don't need to care about it
+  // we only need to make sure cond1 and cond3 are true when we count the triangle (i,j,k)
+  // so we need to check:
+  // A[i] < A[j] + A[k]
+  // A[i] > A[k] - A[j]
+
+  // for each possible i, count the triangles
+  for (let i of _.range(A.length - 2)) {
+    let j = i + 1;
+    let k = i + 2;
+
+    while (k < A.length) {
+      if (A[i] < A[j] + A[k] && A[i] > A[k] - A[j]) {
+        count++;
+        console.log(`triangle (${A[i]}, ${A[j]}, ${A[k]})`);
+      }
+
+      // either move j or k
+      if (A[i] + A[j] <= A[k] || k === A.length - 1) { // move j if A[i] + A[j] <= A[k] or k cannot be moved
+        j++;
+        // after j is moved, make sure k is still > j
+        if (j === k) {
+          k++;
+        }
+      } else {
+        k++;
+      }
+    }
+  }
+  return count;
+};
+
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,8 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should count triangles', () => {
+    const A = [10, 2, 5, 1, 8, 12]; // sorted -> [1, 2, 5, 8, 10, 12]
+    expect(howdy(A)).to.equal(4);
   });
 });
