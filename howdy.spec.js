2a85dd8e3201d4da893762e3d161ae899a03ab14
anglee <ang.lee@gmail.com>
2016-04-02 13:05:01 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,30 @@
 import {expect} from 'chai';
 import howdy from './howdy';
 
+// on the phone pad
+// 1 2 3
+// 4 5 6
+// 7 8 9
+//   0
+
+// assuming one can only move like the knight in chess
+// given 2 numbers A, B on the phone pad, find a shortest path
+// from A to B, if there is no path, return null;
+
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should find the shortest path on the phone pad', () => {
+    expect(howdy(1, 8)).to.eql([1, 8]);
+  });
+  it('should find the shortest path on the phone pad', () => {
+    expect(howdy(1, 3)).to.eql([1, 8, 3]);
+  });
+  it('should find the shortest path on the phone pad', () => {
+    expect(howdy(1, 0)).to.eql([1, 6, 0]);
+  });
+  it('should find the shortest path on the phone pad', () => {
+    expect(howdy(1, 9).length).to.equal([1, 6, 7, 2, 9].length);
+  });
+  it('should return null if there is no path', () => {
+    expect(howdy(1, 5)).to.be.null;
   });
 });
