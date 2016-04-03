baa194abcd45fd6105e52cdbab3642d5081bf834
anglee <ang.lee@gmail.com>
2016-04-03 14:26:30 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,20 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const helper = (A, i, j) => {
+  if (A[j] >= A[i]) {
+    return A[i];
+  }
+
+  const m = Math.floor((i + j) / 2);
+
+  if (A[m] > A[j]) {
+    return helper(A, m + 1, j);
+  } else {
+    return helper(A, i, m);
+  }
+};
+const howdy = (A) => {
+  return helper(A, 0, A.length - 1);
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,33 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [3, 4, 5, 0, 1, 2];
+    expect(howdy(sortedArray)).to.equal(0);
+  });
+
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [1, 2];
+    expect(howdy(sortedArray)).to.equal(1);
+  });
+
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [3, 1];
+    expect(howdy(sortedArray)).to.equal(1);
+  });
+
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [1, 2, 3];
+    expect(howdy(sortedArray)).to.equal(1);
+  });
+
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [2, 3, 1];
+    expect(howdy(sortedArray)).to.equal(1);
+  });
+
+  it('should find the minimum in a sorted array', () => {
+    const sortedArray = [3, 1, 2];
+    expect(howdy(sortedArray)).to.equal(1);
   });
 });
