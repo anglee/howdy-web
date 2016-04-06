6a8a42fd22127aa060c10145acb922a782396d25
anglee <ang.lee@gmail.com>
2016-04-06 08:28:48 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,27 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const getOperation = (it) => {
+  switch (it) {
+    case  '+': return (a, b) => a + b;
+    case  '-': return (a, b) => a - b;
+    case  '*': return (a, b) => a * b;
+    case  '/': return (a, b) => Math.floor(a / b);
+  }
+};
+const solution = (A) => {
+  const stack = [];
+  _.forEach(A, (e) => {
+    const number = parseInt(e);
+    if (!_.isNaN(number)) {
+      stack.push(number);
+    } else {
+      const op2 = stack.pop();
+      const op1 = stack.pop();
+      const operation = getOperation(e);
+      stack.push(operation(op1, op2));
+    }
+  });
+  return stack.pop();
+};
 
-export default howdy;
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,14 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('solution', () => {
+  it('should evaluate Reverse Polish Notation', () => {
+    const input = ["2", "1", "+", "3", "*"];
+    expect(solution(input)).to.equal(9);
+  });
+
+  it('should evaluate Reverse Polish Notation', () => {
+    const input = ["4", "13", "5", "/", "+"];
+    expect(solution(input)).to.equal(6);
   });
 });
