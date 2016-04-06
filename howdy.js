2dd12feed29928462aaf91164fd4790b969a4bb0
anglee <ang.lee@gmail.com>
2016-04-06 07:47:26 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,53 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+export class MinStack{ // extra space: O(n)
+  constructor() {
+    this.elemStack = [];
+    this.minStack = [];
+  }
+  push(e) {
+    this.elemStack.push(e);
+    if (_.isEmpty(this.minStack)) {
+      this.minStack.push(e);
+    } else {
+      this.minStack.push(Math.min(e, this.getMin()));
+    }
+  }
+  pop() {
+    this.minStack.pop();
+    return this.elemStack.pop();
+  }
+  top() {
+    return this.elemStack[this.elemStack.length - 1];
+  }
+  getMin() {
+    return this.minStack[this.minStack.length - 1];
+  }
+};
 
-export default howdy;
+export class MinStack2 { // minor space optimization, space complexity is still O(n)
+  constructor() {
+    this.elemStack = [];
+    this.minStack = [];
+  }
+  push(e) {
+    this.elemStack.push(e);
+    if (_.isEmpty(this.minStack) || e <= _.last(this.minStack)) {
+      this.minStack.push(e);
+    }
+  }
+  pop() {
+    const popped = this.elemStack.pop();
+    if (popped === _.last(this.minStack)) {
+      this.minStack.pop();
+    }
+    return popped;
+  }
+  top() {
+    return _.last(this.elemStack);
+  }
+  getMin() {
+    return _.last(this.minStack);
+  }
+};
+
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,95 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import {MinStack, MinStack2} from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('MinStack', () => {
+  it('should should work', () => {
+    const s = new MinStack();
+    s.push(1);
+    expect(s.top()).to.equal(1);
+    expect(s.getMin()).to.equal(1);
+  });
+  it('should should work', () => {
+    const s = new MinStack();
+    s.push(1);
+    s.push(2);
+    expect(s.top()).to.equal(2);
+    expect(s.getMin()).to.equal(1);
+  });
+  it('should should work', () => {
+    const s = new MinStack();
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    expect(s.top()).to.equal(1);
+    expect(s.getMin()).to.equal(1);
+    expect(s.pop()).to.equal(1);
+    expect(s.top()).to.equal(2);
+    expect(s.getMin()).to.equal(2);
+    expect(s.pop()).to.equal(2);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(3);
+  });
+  it('should should work', () => {
+    const s = new MinStack();
+    s.push(1);
+    s.push(4);
+    s.push(3);
+    s.push(0);
+    s.push(3);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(0);
+    expect(s.pop()).to.equal(3);
+    expect(s.top()).to.equal(0);
+    expect(s.getMin()).to.equal(0);
+    expect(s.pop()).to.equal(0);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(1);
+  });
+});
+
+
+describe('MinStack2', () => {
+  it('should should work', () => {
+    const s = new MinStack2();
+    s.push(1);
+    expect(s.top()).to.equal(1);
+    expect(s.getMin()).to.equal(1);
+  });
+  it('should should work', () => {
+    const s = new MinStack2();
+    s.push(1);
+    s.push(2);
+    expect(s.top()).to.equal(2);
+    expect(s.getMin()).to.equal(1);
+  });
+  it('should should work', () => {
+    const s = new MinStack2();
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    expect(s.top()).to.equal(1);
+    expect(s.getMin()).to.equal(1);
+    expect(s.pop()).to.equal(1);
+    expect(s.top()).to.equal(2);
+    expect(s.getMin()).to.equal(2);
+    expect(s.pop()).to.equal(2);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(3);
+  });
+  it('should should work', () => {
+    const s = new MinStack2();
+    s.push(1);
+    s.push(4);
+    s.push(3);
+    s.push(0);
+    s.push(3);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(0);
+    expect(s.pop()).to.equal(3);
+    expect(s.top()).to.equal(0);
+    expect(s.getMin()).to.equal(0);
+    expect(s.pop()).to.equal(0);
+    expect(s.top()).to.equal(3);
+    expect(s.getMin()).to.equal(1);
   });
 });
