116fad95fa41ee7b5d9242642a9297c6fea3bd84
anglee <ang.lee@gmail.com>
2016-04-20 11:17:26 -0400

    Update howdy.js to be Amz__Start-Template.js

--- howdy.spec.js
+++ howdy.spec.js
@@ -4,11 +4,12 @@ import solution from './howdy';
 describe('howdy', () => {
   it('should work with given sample test', () => {
     const sampleInput = `2
-abdcghbaabcdij bcda
-bbbababaaabbbb ab`;
+3 12
+1 6 9
+5 9
+3 4 4 4 8`;
     const sampleOutput = `12
-2 0 8
-6 2 3 4 5 6 9`;
+9`;
     expect(solution(sampleInput)).to.equal(sampleOutput);
   });
 });
