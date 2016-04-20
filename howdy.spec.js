4228d2643e2e65aa65c55f1409680a92ad12d036
anglee <ang.lee@gmail.com>
2016-04-20 07:53:03 -0400

    Update howdy.js

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,15 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should work with given sample test', () => {
+    const sampleInput = `2
+3 12
+1 6 9
+5 9
+3 4 4 4 8`;
+    const sampleOutput = `12
+9`;
+    expect(solution(sampleInput)).to.equal(sampleOutput);
   });
 });
