b19595081785d0dcf0ea2877aec753cb435d8c0e
anglee <ang.lee@gmail.com>
2016-04-23 11:13:42 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,62 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
+//import fs from 'fs';
+var fs = require('fs');
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+const transformInput = (input) => {
+  const lines = input.split('\n');
+  const N = parseInt(lines[0]);
+  const actions = [];
+  for (var i = 0; i < N; ++i) {
+    const tokens = lines[i + 1].split(' ');
+    if (tokens[0] === '1') {
+      actions.push({
+        type: 'PUSH',
+        item: parseInt(tokens[1])
+      });
+    } else if (tokens[0] === '2') {
+      actions.push({
+        type: 'POP'
+      });
+    } else if (tokens[0] === '3') {
+      actions.push({
+        type: 'PRINT_MAX'
+      });
+    }
+  }
+  return actions;
+};
+
+const transformOutput = (lines) => {
+  return lines.join('\n');
+};
+
+describe('solution', () => {
+  xit('should work with sample case', () => {
+    const sampleInput = `10
+1 97
+2
+1 20
+2
+1 26
+1 20
+2
+3
+1 91
+3`;
+    const sampleOutput = `26
+91`;
+    const input = transformInput(sampleInput);
+    const output = transformOutput(solution((input)));
+    expect(output).to.equal(sampleOutput);
+  });
+
+  it('should work with sample case', () => {
+    const sampleInput = fs.readFileSync('./sampleTest6.input').toString();
+    const sampleOutput = fs.readFileSync('./sampleTest6.output').toString();
+
+    const input = transformInput(sampleInput);
+    const output = transformOutput(solution((input)));
+    expect(output).to.equal(sampleOutput);
   });
 });
