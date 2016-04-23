ae0999e99f81a26f2bf6ba191ad0118554234070
anglee <ang.lee@gmail.com>
2016-04-23 16:39:15 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,54 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+//const Primes = [2, 3, 5, 7, 11, 13];
 
-export default howdy;
+let previousPrimes = [];
+
+const getNextPrime = () => {
+  if (previousPrimes.length === 0) {
+    previousPrimes.push(2);
+    return 2;
+  } else if (previousPrimes.length === 1) {
+    previousPrimes.push(3);
+    return 3;
+  } else {
+    const last = previousPrimes[previousPrimes.length - 1];
+    let i = last + 2;
+    while (true) {
+      if (!_.some(previousPrimes, (pp) => i % pp === 0)) {
+        previousPrimes.push(i);
+        return i;
+      } else {
+        i+=2;
+      }
+    }
+  }
+};
+
+const solution = (Q, plates) => {
+  //const primes = Primes.slice(0, Q);
+  previousPrimes = [];
+
+  let pile = plates;
+  let divisiblePiles = [];
+  for (let i = 0; i < Q; i++) {
+    const prime = getNextPrime();
+    const divisiblePile = [];
+    const lastPile = [];
+    while (!_.isEmpty(pile)) {
+      const plate = pile.pop();
+      if (plate % prime === 0) {
+        divisiblePile.push(plate);
+      } else {
+        lastPile.push(plate);
+      }
+    }
+    divisiblePiles.push(divisiblePile);
+    pile = lastPile;
+  }
+  return [...divisiblePiles, pile].reduce((ret, pi) => {
+    return [...ret, ...pi.reverse()];
+  }, []);
+};
+
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,53 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
+import fs from 'fs';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+const transformInput = (input) => {
+  const lines = input.split('\n');
+  const line1 = lines[0];
+  const line1Tokens = line1.split(' ');
+  const N = parseInt(line1Tokens[0]);
+  const Q = parseInt(line1Tokens[1]);
+  const line2 = lines[1];
+  const plates = line2.split(' ').map((it) => parseInt(it));
+  return {Q, plates}
+};
+
+const transformOutput = (output) => {
+  return output.join('\n');
+};
+describe('solution', () => {
+  it('should work with the sample test', () => {
+    const sampleInput = `5 1
+3 4 7 6 5`;
+    const {Q, plates} = transformInput(sampleInput);
+    const output = transformOutput(solution(Q, plates));
+    const sampleOutput = `4
+6
+3
+7
+5`;
+    expect(output).to.equal(sampleOutput);
+  });
+
+  it('should work with the sample test', () => {
+    const sampleInput = `5 2
+3 3 4 4 9`;
+    const {Q, plates} = transformInput(sampleInput);
+    const output = transformOutput(solution(Q, plates));
+    const sampleOutput = `4
+4
+9
+3
+3`;
+    expect(output).to.equal(sampleOutput);
+  });
+
+  it('should work with the sample test', () => {
+    const sampleInput = fs.readFileSync('./test11.input').toString();
+    const {Q, plates} = transformInput(sampleInput);
+    const output = transformOutput(solution(Q, plates));
+    const sampleOutput = fs.readFileSync('./test11.output').toString();;
+    expect(output).to.equal(sampleOutput);
   });
 });
