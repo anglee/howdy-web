b713f5dcdafde612217ac087306160a14bcee821
anglee <ang.lee@gmail.com>
2016-04-24 15:32:08 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,37 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('solution', () => {
+  it('should work with example', () => {
+    const input = [1, 2, 3, 3, 3, 1, 1, 2, 4];
+    expect(solution(input)).to.equal(9);
+  });
+  it('should work with example', () => {
+    const input = [1, 2, 3, 4, 5];
+    expect(solution(input)).to.equal(9);
+  });
+  it('should work with example', () => {
+    const input = [2, 1, 2, 3, 4, 5];
+    expect(solution(input)).to.equal(9);
+  });
+  it('should work with example', () => {
+    const input = [3, 2, 1, 2, 3, 4, 5];
+    expect(solution(input)).to.equal(9);
+  });
+  it('should work with example', () => {
+    const input = [4, 3, 2, 1, 2, 3, 4, 5];
+    expect(solution(input)).to.equal(9);
+  });
+  it('should work with example', () => {
+    const input = [2, 1, 2];
+    expect(solution(input)).to.equal(3);
+  });
+  it('should work with example', () => {
+    const input = [1, 2, 1];
+    expect(solution(input)).to.equal(3);
+  });
+  it('should work with example', () => {
+    const input = [6, 2, 5, 4, 5, 1, 6];
+    expect(solution(input)).to.equal(12);
   });
 });
