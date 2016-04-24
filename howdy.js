b8193bdcd9f13512fcb30f93a9102016ce8b27fa
anglee <ang.lee@gmail.com>
2016-04-24 16:33:10 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,11 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const howdy = (r1, r2) => {
+  const isNotOverlap = r1.left > r2.right
+    || r1.right < r2.left
+    || r1.top < r2.bottom
+    || r1.bottom > r2.top;
+  return !isNotOverlap;
+};
 
 export default howdy;
--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,64 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should determine if 2 rectangles overlap', () => {
+    const r1 = {
+      left: 0,
+      right: 5,
+      top: 5,
+      bottom: 0
+    };
+    const r2 = {
+      left: 10,
+      right: 15,
+      top: 5,
+      bottom: 0
+    };
+    expect(howdy(r1, r2)).to.equal(false);
+  });
+  it('should determine if 2 rectangles overlap', () => {
+    const r1 = {
+      left: 0,
+      right: 5,
+      top: 5,
+      bottom: 0
+    };
+    const r2 = {
+      left: 1,
+      right: 4,
+      top: 4,
+      bottom: 1
+    };
+    expect(howdy(r1, r2)).to.equal(true);
+  });
+  it('should determine if 2 rectangles overlap', () => {
+    const r1 = {
+      left: 0,
+      right: 5,
+      top: 5,
+      bottom: 0
+    };
+    const r2 = {
+      left: 3,
+      right: 8,
+      top: 8,
+      bottom: 3
+    };
+    expect(howdy(r1, r2)).to.equal(true);
+  });
+  it('should determine if 2 rectangles overlap', () => {
+    const r1 = {
+      left: 0,
+      right: 10,
+      top: 6,
+      bottom: 4
+    };
+    const r2 = {
+      left: 4,
+      right: 6,
+      top: 10,
+      bottom: 0
+    };
+    expect(howdy(r1, r2)).to.equal(true);
   });
 });
