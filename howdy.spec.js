4d945b55b9fc10809c6281fd3590e2ba67824d63
anglee <ang.lee@gmail.com>
2016-04-24 15:54:07 -0400

    Solved

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,17 @@
+import _ from 'lodash';
 import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should generate all combination of a string', () => {
+    const input = 'abc';
+    const actual = howdy(input);
+    const expected = ['a', 'b', 'c',
+    'ab', 'bc', 'ac',
+    'abc'];
+    //console.log('actual', actual);
+    //console.log('expected', expected);
+    expect(_.difference(expected, actual)).to.empty;
+    expect(_.difference(actual, expected)).to.empty;
   });
 });
