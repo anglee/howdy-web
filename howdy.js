21eaae3958b2d4b88dc2da4bc3ccdc855dca675c
anglee <ang.lee@gmail.com>
2016-04-24 16:09:37 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,23 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const doPermute = (prefix, charsSet) => {
+  if (_.isEmpty(charsSet)) {
+    return [prefix];
+  }
+  const result = Array.from(charsSet).map((char) => {
+    const set = new Set(charsSet);
+    set.delete(char);
+    return doPermute(prefix + char, set)
+  });
+  return _.flatten(result);
+};
+
+const permute = (chars) => {
+  return doPermute('', new Set(chars));
+};
+
+const howdy = (string) => {
+  return permute(string.split(''));
+};
 
 export default howdy;
