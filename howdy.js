419cb31e7fc151e0545a3d06dcaf3f88d296608a
Ang Lee <anglee@appnexus.com>
2016-05-11 17:11:20 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,21 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const minPatches = (nums, n) => {
+  let count = 0;
+  let covered = 0;
+  let i = 0;
 
-export default howdy;
+  while (covered < n) {
+    if (i >= nums.length || nums[i] > covered + 1) {
+      count++;
+      const patch = covered + 1;
+      covered = patch + covered;
+    } else {
+      covered = nums[i] + covered;
+      i++;
+    }
+  }
+  return count;
+};
+
+export default minPatches;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,20 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import minPatches from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('minPatches', () => {
+  it('find the minimum patches needed to fill range [1..n]', () => {
+    const nums = [1, 3];
+    const n = 6;
+    expect(minPatches(nums, n)).to.equal(1);
+  });
+  it('find the minimum patches needed to fill range [1..n]', () => {
+    const nums = [1, 5, 10];
+    const n = 20;
+    expect(minPatches(nums, n)).to.equal(2);
+  });
+  it('find the minimum patches needed to fill range [1..n]', () => {
+    const nums = [1, 2, 2];
+    const n = 5;
+    expect(minPatches(nums, n)).to.equal(0);
   });
 });
