2f5a55989e567d6d3244ee3357f7361f23dad4e1
anglee <ang.lee@gmail.com>
2016-05-14 19:32:51 -0400

    Solved

--- howdy.js
+++ howdy.js
@@ -1,5 +1,51 @@
 import _ from 'lodash';
+//const _ = require('lodash');
 
-const howdy = "Howdy, World!";
+const p = (obj) => {
+  var ret = '{';
+  _.forEach(obj, (v, k) => {
+    ret += `${k}: ${v}, `;
+  })
+  return ret + '}';
+};
 
-export default howdy;
+const isMatch = (pattern, str, patternMap) => {
+  //console.log(`isMatch(${pattern}, ${str}), ${p(patternMap)}`);
+  if (pattern.length === 0 && str.length === 0) {
+    return true;
+  }
+  if (pattern.length === 0 || str.length === 0) {
+    return false;
+  }
+  const firstChar = pattern[0];
+  if (patternMap[firstChar]) {
+    if (str.indexOf(patternMap[firstChar]) === 0) {
+      return isMatch(
+        pattern.substr(1),
+        str.substr(patternMap[firstChar].length),
+        patternMap);
+    } else {
+      return false
+    }
+  } else {
+    for (var l = 1; l <= str.length; l++) {
+      const newPatternMap = Object.assign({}, patternMap, {
+        [firstChar]: str.substr(0, l)
+      });
+      if (isMatch(
+          pattern.substr(1),
+          str.substr(l),
+          newPatternMap)) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+const solution = (pattern, str) => {
+  return isMatch(pattern, str, {});
+};
+
+//console.log(isMatch('ba', 'catdog', {a: 'dog', b: 'cat'}));
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,40 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import solution from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should match pattern', () => {
+    const pattern = '';
+    const str = '';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'a';
+    const str = 'abc';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'aa';
+    const str = 'abcabc';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'ab';
+    const str = 'xyz';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'abba';
+    const str = 'dogcatcatdog';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'abab';
+    const str = 'dogcatdogcat';
+    expect(solution(pattern, str)).to.be.true;
+  });
+  it('should match pattern', () => {
+    const pattern = 'abab';
+    const str = 'dogcatdogfish';
+    expect(solution(pattern, str)).to.be.false;
   });
 });
