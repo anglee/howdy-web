d7fedffd0edd9218a91db87eda4ffedbd1dfbc1c
anglee <ang.lee@gmail.com>
2016-05-29 16:38:59 -0400

    Classic_2-3-Tree

--- howdy.js
+++ howdy.js
@@ -1,5 +1,147 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+function TreeNode(l = null, v = null, r = null) {
+  this.value = v;
+  this.value1 = null;
+  this.left = l;
+  this.middle = null;
+  this.right = r;
 
-export default howdy;
+  const isLeaf = () => {
+    if (isEmpty()) {
+      return false;
+    }
+    return this.left === null
+      && this.middle === null
+      && this.right === null;
+  };
+  const isEmpty = () => {
+    return this.value === null;
+  };
+  const is2Node = () => {
+    return this.value !== null && this.value1 === null;
+  };
+  const is3Node = () => {
+    return this.value !== null && this.value1 !== null;
+  };
+  const search = (value) => {
+    if (isLeaf()) {
+      return value === this.value || value === this.value1;
+    } else {
+      if (is2Node()) {
+        if (value < this.value) {
+          return this.left.search(value);
+        } else {
+          return this.right.search(value);
+        }
+      } else { // is3Node
+        if (value < this.value) {
+          return this.left.search(value);
+        } else if (value < this.value1) {
+          return this.middle.search(value);
+        } else {
+          return this.right.search(value);
+        }
+      }
+    }
+  };
+  const set3Values = (v1, v2, v3) => {
+    this.left = v1;
+    this.value = v2;
+    this.middle = null;
+    this.value1 = null;
+    this.right = v3;
+  };
+  const set5Values = (v1, v2, v3, v4, v5) => {
+    this.left = v1;
+    this.value = v2;
+    this.middle = v3;
+    this.value1 = v4;
+    this.right = v5;
+  };
+  const set7Values = (v1, v2, v3, v4, v5, v6, v7) => {
+    //console.log(`set7Values`, v1, v2, v3, v4, v5, v6, v7);
+    const newLeftNode = new TreeNode(v1, v2, v3);
+    const newRightNode = new TreeNode(v5, v6, v7);
+    set3Values(newLeftNode, v4, newRightNode);
+  };
+  const insert = (value) => {
+    if (isEmpty()) {
+      //console.log('insert to empty node', value);
+      set3Values(null, value, null);
+    } else if (is2Node()) {
+      //console.log('insert to 2-node', value);
+      if (isLeaf()) {
+        if (value < this.value) {
+          set5Values(null, value, null, this.value, null);
+        } else { // value >= this.value
+          set5Values(null, this.value, null, value, null);
+        }
+      } else {
+        if (value < this.value) {
+          const ret = this.left.insert(value);
+          if (ret) {
+            set5Values(ret.left, ret.value, ret.right, this.value, this.right);
+          }
+        } else { // value >= this.value
+          const ret = this.right.insert(value);
+          if (ret) {
+            set5Values(this.left, this.value, ret.left, ret.value, ret.right);
+          }
+        }
+      }
+      return null;
+    } else { // is3Node
+      //console.log('insert to 3-node', value);
+      if (isLeaf()) {
+        if (value < this.value) {
+          set7Values(null, value, null, this.value, null, this.value1, null);
+        } else if (value < this.value1) {
+          set7Values(null, this.value, null, value, null, this.value1, null);
+        } else { // value >= this.value1
+          set7Values(null, this.value, null, this.value1, null, value, null);
+        }
+      } else {
+        if (value < this.value) {
+          const ret = this.left.insert(value);
+          if (ret) {
+            set7Values(ret.left, ret.value, ret.right, this.value, this.middle, this.value1, this.right);
+          }
+        } else if (value < this.value1) {
+          const ret = this.middle.insert(value);
+          if (ret) {
+            set7Values(this.left, this.value, ret.left, ret.value, ret.right, this.value1, this.right);
+          }
+        } else {
+          const ret = this.right.insert(value);
+          if (ret) {
+            set7Values(this.left, this.value, this.middle, this.value1, ret.left, ret.value, ret.right);
+          }
+        }
+      }
+      return this;
+    }
+  };
+
+  const getValues = () => {
+    if (isEmpty()) {
+      return [];
+    } else if (is2Node()) {
+      return [this.value];
+    } else { // is3Node
+      return [this.value, this.value1];
+    }
+  };
+  Object.assign(this, {
+    search,
+    insert,
+    getValues,
+    isLeaf,
+    isEmpty,
+    is2Node,
+    is3Node
+  });
+  return this;
+}
+
+export {TreeNode};
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,111 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import {TreeNode} from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+  it('should identify empty tree', () => {
+    const root = new TreeNode();
+    expect(root.getValues()).to.deep.equal([]);
+
+    expect(root.left).to.be.null;
+    expect(root.value).to.be.null;
+    expect(root.middle).to.be.null;
+    expect(root.value1).to.be.null;
+    expect(root.right).to.be.null;
+    expect(root.isLeaf()).to.be.false;
+    expect(root.isEmpty()).to.be.true;
+    expect(root.is2Node()).to.be.false;
+    expect(root.is3Node()).to.be.false;
+  });
+  it('should identify single 2-node tree', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    expect(root.getValues()).to.deep.equal([9]);
+
+    expect(root.left).to.be.null;
+    expect(root.value).to.equal(9);
+    expect(root.middle).to.be.null;
+    expect(root.value1).to.be.null;
+    expect(root.right).to.be.null;
+    expect(root.isLeaf()).to.be.true;
+    expect(root.isEmpty()).to.be.false;
+    expect(root.is2Node()).to.be.true;
+    expect(root.is3Node()).to.be.false;
+  });
+  it('should identify single 3-node tree', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    expect(root.getValues()).to.deep.equal([5,9]);
+
+    expect(root.left).to.be.null;
+    expect(root.value).to.equal(5);
+    expect(root.middle).to.be.null;
+    expect(root.value1).to.equal(9);
+    expect(root.right).to.be.null;
+    expect(root.isLeaf()).to.be.true;
+    expect(root.isEmpty()).to.be.false;
+    expect(root.is2Node()).to.be.false;
+    expect(root.is3Node()).to.be.true;
+  });
+  it('should handle inserting 3-node leaf node', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    root.insert(8);
+    expect(root.getValues()).to.deep.equal([8]);
+    expect(root.left.getValues()).to.deep.equal([5]);
+    expect(root.right.getValues()).to.deep.equal([9]);
+  });
+  it('should handle inserting', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    root.insert(8);
+    root.insert(3);
+    expect(root.getValues()).to.deep.equal([8]);
+    expect(root.left.getValues()).to.deep.equal([3,5]);
+    expect(root.right.getValues()).to.deep.equal([9]);
+  });
+  it('should handle inserting', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    root.insert(8);
+    root.insert(3);
+    root.insert(2);
+    expect(root.getValues()).to.deep.equal([3,8]);
+    expect(root.left.getValues()).to.deep.equal([2]);
+    expect(root.middle.getValues()).to.deep.equal([5]);
+    expect(root.right.getValues()).to.deep.equal([9]);
+  });
+  it('should handle inserting', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    root.insert(8);
+    root.insert(3);
+    root.insert(2);
+    root.insert(4);
+    expect(root.getValues()).to.deep.equal([3,8]);
+    expect(root.left.getValues()).to.deep.equal([2]);
+    expect(root.middle.getValues()).to.deep.equal([4,5]);
+    expect(root.right.getValues()).to.deep.equal([9]);
+  });
+  it('should handle inserting', () => {
+    const root = new TreeNode();
+    root.insert(9);
+    root.insert(5);
+    root.insert(8);
+    root.insert(3);
+    root.insert(2);
+    root.insert(4);
+    root.insert(7);
+    expect(root.getValues()).to.deep.equal([5]);
+    expect(root.left.getValues()).to.deep.equal([3]);
+    expect(root.right.getValues()).to.deep.equal([8]);
+    expect(root.left.left.getValues()).to.deep.equal([2]);
+    expect(root.left.right.getValues()).to.deep.equal([4]);
+    expect(root.right.left.getValues()).to.deep.equal([7]);
+    expect(root.right.right.getValues()).to.deep.equal([9]);
   });
 });
