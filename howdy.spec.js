e0b66ec9a3752ce006c5a473a91c0979f3e08e94
anglee <ang.lee@gmail.com>
2016-05-30 19:11:47 -0400

    leetOJ-325_Maximum-Size-Subarray-Sum-Equals-k

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,17 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import maxSubArrayLen from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('leetOJ-325_Maximum-Size-Subarray-Sum-Equals-k', () => {
+  describe('maxSubArrayLen', () => {
+    it('should work with given example', () => {
+      const k = 3;
+      const actual = maxSubArrayLen([1, -1, 5, -2, 3], k);
+      expect(actual).to.equal(4);
+    });
+    it('should work with given example', () => {
+      const k = 1;
+      const actual = maxSubArrayLen([-2, -1, 2, 1], k);
+      expect(actual).to.equal(2);
+    });
   });
 });
