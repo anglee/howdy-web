ff66d8febb70d14bacda55b668579486d6742a4e
anglee <ang.lee@gmail.com>
2016-06-07 14:54:30 -0400

    leetOJ-150_Evaluate-Reverse-Polish-Notation

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,15 @@
-import {expect} from 'chai';
-import howdy from './howdy';
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
-describe('howdy', () => {
+import {expect} from 'chai';
+import evalRPN from './howdy';
+
+describe('rpn', () => {
   it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+    const input = ["2", "1", "+", "3", "*"];
+    expect(evalRPN(input)).to.equal(9);
+  });
+  it('should say "Howdy, World!"', () => {
+    const input = ["4", "13", "5", "/", "+"];
+    expect(evalRPN(input)).to.equal(6);
   });
 });
