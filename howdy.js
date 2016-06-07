f4b8ff8fd65b6b77fc5b08aae7cd7c8329f5c6e4
anglee <ang.lee@gmail.com>
2016-06-07 07:33:14 -0400

    leetOJ-156_Binary-Tree-UpsideDown

--- howdy.js
+++ howdy.js
@@ -1,5 +1,19 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
+const upsideDownBinaryTree = (root) => {
+  let prev = null;
+  let node = root;
+  let prevRight = null;
+  while (node) {
+    let left = node.left;
+    let right = node.right;
+    node.left = prevRight;
+    node.right = prev;
+    prev = node;
+    prevRight = right;
+    node = left;
+  }
+  return prev;
+};
 
-export default howdy;
+export default upsideDownBinaryTree;
