4728a3fbb6861f9268bb7a93a877b0ce88fe61eb
anglee <ang.lee@gmail.com>
2016-06-22 07:06:09 -0400

    JumpTrading__Knight-Pattern

--- howdy.js
+++ howdy.js
@@ -1,5 +1,88 @@
 import _ from 'lodash';
 
-const howdy = "Howdy, World!";
 
-export default howdy;
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+// I did notice some pattern in the grid, and it is symmetric along the x, y axis as well as
+// the 45 degree diagnols, (x = y, x = -y), but I was not able to come up with a formula
+// that can give me O(1) time and O(1) space, so I falled back to implement this BFS based
+// solution, which is O(N^2) time and O(N^2) space, where N is the max of A and B
+
+const BFS = (A, B) => {
+  const q = [{
+    x: 0, y: 0, parentSteps: -1
+  }];
+  const map = new Map2D();
+
+  const getChildren = (node) => {
+    var steps = map.get(node.x, node.y);
+
+    return [[1,2], [2,1],
+      [-1,2], [-2,1],
+      [1,-2], [2,-1],
+      [-1,-2], [-2,-1]].map((xy) => {
+      return {
+        x: node.x + xy[0],
+        y: node.y + xy[1],
+        parentSteps: steps
+      };
+    });
+  };
+
+  while (q.length > 0 && q.length <=4000) {
+    var node = q.shift();
+    if (node.x === A && node.y === B) {
+      return node.parentSteps + 1;
+    }
+    if (!map.has(node.x, node.y)) {
+      if (node.parentSteps > 10e8) {
+        return -2;
+      }
+      map.set(node.x, node.y, node.parentSteps + 1);
+    }
+    getChildren(node).forEach((child) => {
+      if (!map.has(child.x, child.y)) {
+        q.push(child);
+      }
+    });
+  }
+};
+
+function Map2D() {
+  const m = new Map();
+
+  // because of the symmetric nature, only we only need to keep entries in the map, where
+  // the key pair [x, y] such that x >= y >= 0
+  function processXY(x, y) {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    // ensure x >= y
+    return (x > y) ? {x: x, y: y} : {x: y, y: x};
+  }
+
+  this.has = (x, y) => {
+    var p = processXY(x, y);
+    return m.has(p.x) && m.get(p.x).has(p.y);
+  };
+  this.set = (x, y, step) => {
+    var p = processXY(x, y);
+    if (!m.has(p.x)) {
+      m.set(p.x, new Map())
+    }
+    m.get(p.x).set(p.y, step);
+  };
+
+  this.get = (x, y) => {
+    var p = processXY(x, y);
+    return m.get(p.x).get(p.y);
+  };
+}
+
+function solution(A, B) {
+  // write your code in JavaScript (Node.js 4.0.0)
+  return BFS(A, B);
+}
+
+
+export default solution;
--- howdy.spec.js
+++ howdy.spec.js
@@ -3,6 +3,48 @@ import howdy from './howdy';
 
 describe('howdy', () => {
   it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+    const A = 4;
+    const B = 5;
+    expect(howdy(A, B)).to.equal(3);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = 2;
+    const B = 1;
+    expect(howdy(A, B)).to.equal(1);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = 3;
+    const B = 3;
+    expect(howdy(A, B)).to.equal(2);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = -1;
+    const B = -2;
+    expect(howdy(A, B)).to.equal(1);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = -3;
+    const B = -3;
+    expect(howdy(A, B)).to.equal(2);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = -4;
+    const B = -5;
+    expect(howdy(A, B)).to.equal(3);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = -5;
+    const B = 4;
+    expect(howdy(A, B)).to.equal(3);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = 0;
+    const B = 1;
+    expect(howdy(A, B)).to.equal(3);
+  });
+  it('should say "Howdy, World!"', () => {
+    const A = 0;
+    const B = 2;
+    expect(howdy(A, B)).to.equal(2);
   });
 });
