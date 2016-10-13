b2107eaabd42e4f54e4ac34942132b7a027c6cc9
anglee <ang.lee@gmail.com>
2016-10-13 08:27:56 -0400

    random_word-contains-letters

--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,35 @@
 import {expect} from 'chai';
-import howdy from './howdy';
+import { onlyContainsLetter } from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('onlyContainsLetter', () => {
+  it('`abc` contains only `abc`', () => {
+    const word = 'abc';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.true;
+  });
+  it('`ac` contains only `abc`', () => {
+    const word = 'ac';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.true;
+  });
+  it('`a` contains only `abc`', () => {
+    const word = 'a';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.true;
+  });
+  it('`abcabc` contains only `abc`', () => {
+    const word = 'abcabc';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.true;
+  });
+  it('`abcd` does not contains only `abc`', () => {
+    const word = 'abcd';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.false;
+  });
+  it('`aah` does not contains only `abc`', () => {
+    const word = 'aah';
+    const letters = 'abc'.split('');
+    expect(onlyContainsLetter(word, letters)).to.be.false;
   });
 });
