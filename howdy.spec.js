65c7818520956cdf6363167a4ccf3b284989c74e
anglee <ang.lee@gmail.com>
2016-11-18 07:39:57 -0500

    AppNexues printing slashes

--- howdy.spec.js
+++ howdy.spec.js
@@ -2,7 +2,103 @@ import {expect} from 'chai';
 import howdy from './howdy';
 
 describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+
+  it('test case 1', () => {
+    const input = `\\/\\/\\/\\/\\/`;
+    expect(howdy(input)).to.equal(`\\/\\/\\/\\/\\/`);
+  });
+
+  it('test case 2', () => {
+    const input = `/\\/\\/\\/\\/\\`;
+    expect(howdy(input)).to.equal(`/\\/\\/\\/\\/\\`);
+  });
+
+  it('test case 3', () => {
+    const input = ``;
+    expect(howdy(input)).to.equal(``);
+  });
+  it('test case 4', () => {
+    const input = `\\\\\\\\////`;
+    expect(howdy(input)).to.equal(`\\      /
+ \\    / 
+  \\  /  
+   \\/   `);
+  });
+  it('test case 4 - 2', () => {
+    const input = `////\\\\\\\\`;
+    expect(howdy(input)).to.equal(`   /\\   
+  /  \\  
+ /    \\ 
+/      \\`);
+  });
+  it('test case 5 - 1', () => {
+    const input = `\\\\\\\\\\\\\\\\////`;
+    expect(howdy(input)).to.equal(`\\           
+ \\          
+  \\         
+   \\        
+    \\      /
+     \\    / 
+      \\  /  
+       \\/   `);
+  });
+  it('test case 5 - 2', () => {
+    const input = `////\\\\\\\\\\\\\\\\`;
+    expect(howdy(input)).to.equal(`   /\\       
+  /  \\      
+ /    \\     
+/      \\    
+        \\   
+         \\  
+          \\ 
+           \\`);
+  });
+  it('test case 6 - 1', () => {
+    const input = `\\\\\\\\////////`;
+    expect(howdy(input)).to.equal(`           /
+          / 
+         /  
+        /   
+\\      /    
+ \\    /     
+  \\  /      
+   \\/       `);
+  });
+  it('test case 6 - 2', () => {
+    const input = `////////\\\\\\\\`;
+    expect(howdy(input)).to.equal(`       /\\   
+      /  \\  
+     /    \\ 
+    /      \\
+   /        
+  /         
+ /          
+/           `);
+  });
+
+  it('test case 7 - 1', () => {
+    const input = `\\\\//\\\\//`;
+    expect(howdy(input)).to.equal(`\\  /\\  /
+ \\/  \\/ `);
+  });
+
+  it('test case 7 - 2', () => {
+    const input = `//\\\\//\\\\`;
+    expect(howdy(input)).to.equal(` /\\  /\\ 
+/  \\/  \\`);
+  });
+
+  it('test case 8', () => {
+    const input = `\\\\\\`;
+    expect(howdy(input)).to.equal(`\\  
+ \\ 
+  \\`);
+  });
+
+  it('test case 9', () => {
+    const input = `///`;
+    expect(howdy(input)).to.equal(`  /
+ / 
+/  `);
   });
 });
