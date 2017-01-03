0be7474a7e1ed4aa0a22466f0e38f4ffffb78349
anglee <ang.lee@gmail.com>
2017-01-03 08:41:18 -0500

    Minor

--- leetOJ/001-TwoSum.spec.js
+++ leetOJ/001-TwoSum.spec.js
@@ -1,15 +1,12 @@
 import {expect} from 'chai';
 import twoSum from './001-TwoSum';
 describe('LeetOJ 001-TwoSum', () => {
-  describe('twoSum', () => {
-    it('should work', () => {
-      const nums = [2, 7, 11, 15];
-      const target = 9;
-      const actual = twoSum(nums, target);
-      const expected = [0, 1];
-      expect(actual).to.eql(expected);
-    });
+  it('twoSum', () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const actual = twoSum(nums, target);
+    const expected = [0, 1];
+    expect(actual).to.eql(expected);
   });
-
 });
 
