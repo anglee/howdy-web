00571f08a7eef8c6d01913a98f7a423e8d80d105
anglee <ang.lee@gmail.com>
2017-01-08 12:01:26 -0500

    Add test case to Binary Search

--- Classic_Binary-Search.spec.js
+++ Classic_Binary-Search.spec.js
@@ -9,6 +9,20 @@ describe('Classic_Binary-Search', () => {
     expect(binarySearch(A, target)).to.equal(A.indexOf(4));
   });
 
+  it('find the index of the first element that is greater than target, ret at the beginning', () => {
+    const A = [1,2,3,4,6];
+    const target = 0;
+    // 1 is the first element in A that A[i] > 0
+    expect(binarySearch(A, target)).to.equal(A.indexOf(1));
+  });
+
+  it('find the index of the first element that is greater than target, ret at the end', () => {
+    const A = [1,2,3,4,6];
+    const target = 5;
+    // 6 is the first element in A that A[i] > 5
+    expect(binarySearch(A, target)).to.equal(A.indexOf(6));
+  });
+
   describe('should return -1 when there is no element in A > target', () => {
     it('should work with non-empty A', () => {
       const A = [1,2,3,4];
