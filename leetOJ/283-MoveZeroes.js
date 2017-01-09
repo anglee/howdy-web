92408b8ac57dc21c00efffc34cb97bbb1a215665
anglee <ang.lee@gmail.com>
2017-01-09 07:19:44 -0500

    leetOJ/283-MoveZeroes.js

---
+++ leetOJ/283-MoveZeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+
+  let p = 0;
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] !== 0) {
+      nums[p++] = nums[i];
+    }
+  }
+  while (p < nums.length) {
+    nums[p++] = 0;
+  }
+
+};
+
+export default moveZeroes;
---
+++ leetOJ/283-MoveZeros.spec.js
@@ -0,0 +1,35 @@
+import {expect} from 'chai';
+import moveZeroes from './283-MoveZeroes';
+
+describe('LeetOJ 283-MoveZeroes', () => {
+  describe('moveZeroes', () => {
+    it('should work with given example', () => {
+      const nums = [0, 1, 0, 3, 12];
+      moveZeroes(nums);
+      const expected = [1, 3, 12, 0, 0];
+      expect(nums).to.eql(expected);
+    });
+
+    it('should work with all zero', () => {
+      const nums = [0, 0, 0];
+      moveZeroes(nums);
+      const expected = [0, 0, 0];
+      expect(nums).to.eql(expected);
+    });
+
+    it('should work with all non-zero', () => {
+      const nums = [1, 3, 2];
+      moveZeroes(nums);
+      const expected = [1, 3, 2];
+      expect(nums).to.eql(expected);
+    });
+
+    it('should work with empty', () => {
+      const nums = [];
+      moveZeroes(nums);
+      const expected = [];
+      expect(nums).to.eql(expected);
+    });
+  });
+});
+
