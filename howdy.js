31b40aac1403f4752585deda542e48875189e0ba
Ang Lee <ang.lee@gmail.com>
2017-01-23 21:45:03 -0500

    WIP

--- howdy.js
+++ howdy.js
@@ -1,5 +1,91 @@
-import _ from 'lodash';
+const printAll0 = function (num) {
+  if (num.length === 1) {
+    return [num];
+  }
+  const ret = [num];
+  for (let i = 1; i < num.length; ++i) {
+    const digit = num.substring(0, i);
+    const rest = num.substring(i);
+    const printedRest = printAll0(rest);
+    ret.push(
+      ...printedRest.map(it => `${digit}+${it}`),
+      ...printedRest.map(it => `${digit}-${it}`),
+      ...printedRest.map(it => `${digit}*${it}`)
+    );
+  }
+  return ret;
+};
+const printAll1 = function (num) {
+  if (num.length === 1) {
+    return [num];
+  }
+  const head = num[0];
+  const rest = num.substr(1);
+  const ret = [];
+  const printAllRest = printAll1(rest);
+  printAllRest.forEach((it) => {
+    ret.push(`${head}${it}`);
+    ret.push(`${head}+${it}`);
+    ret.push(`${head}-${it}`);
+    ret.push(`${head}*${it}`);
+  });
+  return ret;
+};
 
-const howdy = "Howdy, World!";
+export const printAll = function (num) {
+  const ret = [];
 
-export default howdy;
+  const printAllI = (prefix, pos) => {
+    if (pos === num.length) {
+      ret.push(prefix);
+      return;
+    }
+    const digit = num[pos];
+    if (pos === 0) {
+      printAllI(`${digit}`, pos + 1);
+    } else {
+      printAllI(`${prefix}${digit}`, pos + 1);
+      printAllI(`${prefix}+${digit}`, pos + 1);
+      printAllI(`${prefix}-${digit}`, pos + 1);
+      printAllI(`${prefix}*${digit}`, pos + 1);
+    }
+  };
+
+  printAllI('', 0);
+  return ret;
+};
+
+/** 
+ * @param {string} num 
+ * @param {number} target  * @return {string[]} 
+ */
+var addOperators = function (num, target) {
+  const ret = [];
+
+  const evaluate = (expression) => {
+    const tokens = expression.split(/[\*\+-]/);
+
+  };
+
+  const printAllI = (prefix, pos) => {
+    if (pos === num.length) {
+      if (evaluate(prefix) === target) {
+        ret.push(prefix);
+      }
+      return;
+    }
+    const digit = num[pos];
+    if (pos === 0) {
+      printAllI(`${digit}`, pos + 1);
+    } else {
+      printAllI(`${prefix}${digit}`, pos + 1);
+      printAllI(`${prefix}+${digit}`, pos + 1);
+      printAllI(`${prefix}-${digit}`, pos + 1);
+      printAllI(`${prefix}*${digit}`, pos + 1);
+    }
+  };
+
+  printAllI('', 0);
+  return ret;
+};
+export default addOperators; 
--- howdy.spec.js
+++ howdy.spec.js
@@ -1,8 +1,56 @@
-import {expect} from 'chai';
-import howdy from './howdy';
+import { expect } from 'chai';
+import addOperators, { printAll } from './howdy';
 
-describe('howdy', () => {
-  it('should say "Howdy, World!"', () => {
-    expect(howdy).to.equal('Howdy, World');
+describe('LeetOJ 282-ExpressionAddOperators', () => {
+
+  describe('printAll', () => {
+    it('should print all possible expressions', () => {
+      expect(new Set(printAll('123'))).to.eql(new Set([
+        '123',
+        '1+23',
+        '1-23',
+        '1*23',
+        '12+3',
+        '1+2+3',
+        '1-2+3',
+        '1*2+3',
+        '12-3',
+        '1+2-3',
+        '1-2-3',
+        '1*2-3',
+        '12*3',
+        '1+2*3',
+        '1-2*3',
+        '1*2*3',
+      ]));
+    });
+  });
+
+  describe('addOperators', () => {
+    it('should work with the given example 1', () => {
+      expect(new Set(addOperators('123', 6))).to.eql(new Set([
+        '1+2+3',
+        '1*2*3',
+      ]));
+    });
+    it('should work with the given example 2', () => {
+      expect(new Set(addOperators('232', 8))).to.eql(new Set([
+        '2*3+2',
+        '2+3*2',
+      ]));
+    });
+    it('should work with the given example 3', () => {
+      expect(new Set(addOperators('105', 5))).to.eql(new Set([
+        '1*0+5',
+        '10-5',
+      ]));
+    });
+    it('should work with the given example 4', () => {
+      expect(new Set(addOperators('00', 0))).to.eql(new Set([
+        '0+0',
+        '0-0',
+        '0*0',
+      ]));
+    });
   });
 });
