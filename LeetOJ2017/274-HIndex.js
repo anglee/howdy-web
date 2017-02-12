1a6d5be90a47eedb27337749646a4577b57d7c97
anglee <ang.lee@gmail.com>
2017-02-12 16:08:18 -0500

    LeetOJ2017/274-HIndex.js

--- LeetOJ2017/274-HIndex.js
+++ LeetOJ2017/274-HIndex.js
@@ -3,8 +3,14 @@
  * @return {number}
  */
 var hIndex = function(citations) {
-  for (let h = citations.length; h >= 1; --h) {
-    if (citations[citations.length - h] >= h) {
+  citations.sort((a, b) => (b - a));
+  // case h === citations.length;
+  let h = citations.length;
+  if (citations[h - 1] >= h) {
+    return h;
+  }
+  for (let h = citations.length - 1; h >= 1; --h) {
+    if (citations[h - 1] >=h && citations[h] <= h) {
       return h;
     }
   }
