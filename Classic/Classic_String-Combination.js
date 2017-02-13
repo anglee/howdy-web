f8ba2eb8dab953c26db67427c4aae255a938423e
anglee <ang.lee@gmail.com>
2017-02-13 08:47:45 -0500

    Update Classic/Classic_String-Combination.js

--- Classic/Classic_String-Combination.js
+++ Classic/Classic_String-Combination.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const doCombine = (prefix, chars) => {
   if (_.isEmpty(chars)) {
-    return prefix === '' ? [] : [prefix];
+    return [prefix];
   }
   const char = _.head(chars);
   const tail = _.tail(chars);
@@ -16,11 +16,50 @@ const combine = (chars) => {
   return doCombine('', chars);
 };
 
-const howdy = (string) => {
+const howdy0 = (string) => {
   return combine(string.split(''));
 };
 
-const input = 'abc';
-const actual = howdy(input);
+const howdy1 = (s) => {
+  if (s.length === 0) {
+    return [''];
+  }
+  const h = s.charAt(0);
+  const rest = howdy(s.substr(1));
+  const ret = [];
+  for (let i = 0; i < rest.length; ++i) {
+    ret.push(`${h}${rest[i]}`);
+    ret.push(rest[i]);
+  }
+  return ret;
+};
+
+
+const howdy2 = (s) => {
+  const getCombinations = (prefix, chars) => {
+    if (chars.length === 0) {
+      return [prefix];
+    }
+    return [
+      ...getCombinations(prefix + chars[0], chars.substr(1)),
+      ...getCombinations(prefix, chars.substr(1)),
+    ];
+  };
+  return getCombinations('', s);
+};
+
+const howdy = (s) => {
+  const ret = [];
+  const combine = (prefix, chars) => {
+    if (chars.length === 0) {
+      ret.push(prefix);
+      return;
+    }
+    combine(prefix + chars[0], chars.substr(1));
+    combine(prefix, chars.substr(1));
+  };
+  combine('', s);
+  return ret;
+};
 
 export default howdy;
--- Classic/Classic_String-Combination.spec.js
+++ Classic/Classic_String-Combination.spec.js
@@ -3,12 +3,33 @@ import {expect} from 'chai';
 import howdy from './Classic_String-Combination';
 
 describe('Classic_String-Combination', () => {
-  it('should generate all combination of a string', () => {
+  it('should generate all combination of a string, 1', () => {
+    const input = 'a';
+    const actual = howdy(input);
+    const expected = ['a', ''];
+    //console.log('actual', actual);
+    //console.log('expected', expected);
+    expect(_.difference(expected, actual)).to.empty;
+    expect(_.difference(actual, expected)).to.empty;
+  });
+  it('should generate all combination of a string, 2', () => {
+    const input = 'ab';
+    const actual = howdy(input);
+    const expected = ['ab', 'a', 'b', ''];
+    //console.log('actual', actual);
+    //console.log('expected', expected);
+    expect(_.difference(expected, actual)).to.empty;
+    expect(_.difference(actual, expected)).to.empty;
+  });
+  it('should generate all combination of a string, 3', () => {
     const input = 'abc';
     const actual = howdy(input);
-    const expected = ['a', 'b', 'c',
-    'ab', 'bc', 'ac',
-    'abc'];
+    const expected = [
+      '',
+      'a', 'b', 'c',
+      'ab', 'bc', 'ac',
+      'abc'
+    ];
     //console.log('actual', actual);
     //console.log('expected', expected);
     expect(_.difference(expected, actual)).to.empty;
