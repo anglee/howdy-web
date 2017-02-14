377d93e1549a42025fcbe678d663f3151da82932
anglee <ang.lee@gmail.com>
2017-02-14 08:12:30 -0500

    LeetOJ2017/077-Combinations.js

---
+++ LeetOJ2017/077-Combinations.js
@@ -0,0 +1,54 @@
+const range = (i, j) => {
+  const ret = [];
+  for (let x = i; x < j; ++x) {
+    ret.push(x);
+  }
+  return ret;
+};
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine0 = function(n, k) {
+  if (n === k) {
+    return [range(1, n + 1)];
+  }
+  if (k === 0) {
+    return [[]];
+  }
+  return [
+    ...combine(n - 1, k - 1).map(it => [n, ...it]),
+    ...combine(n - 1, k)
+  ];
+};
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) { // DP
+  let buffer0 = new Array(n).fill([[]]);
+  let buffer1 = new Array(n).fill([]);
+
+  for (let ki = 1; ki <= k; ++ki) {
+    buffer1[ki] = [range(1, ki + 1)];
+    for (let ni = ki + 1; ni < n; ++ni) {
+      buffer1[ni] = [
+        ...buffer0[ni - 1].map(it => [...it, ni]),
+        ...buffer1[ni - 1]
+      ];
+    }
+    if (ki !== k) {
+      buffer0 = buffer1;
+      buffer1 = new Array(n).fill([]);
+    }
+  }
+  return [
+    ...buffer0[n - 1].map(it => [...it, n]),
+    ...buffer1[n - 1]
+  ];
+};
+
+export default combine;
--- LeetOJ2017/077-Combinations.spec.js
+++ LeetOJ2017/077-Combinations.spec.js
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
-import combine from './leetOJ-077_Combinations';
+import combine from './077-Combinations';
+
 
 const process = (A) => {
   A.forEach(it => { it.sort((a, b) => a - b); });
@@ -17,18 +18,24 @@ const process = (A) => {
   return A;
 };
 
-describe('leetOJ-077_Combinations', () => {
+describe('LeetOJ 077-Combinations', () => {
+
   describe('combine', () => {
     it('should solve the given example', () => {
-      const expected = [
-        [2,4],
-        [3,4],
-        [2,3],
-        [1,2],
-        [1,3],
-        [1,4],
-      ];
       const actual = combine(4, 2);
+      const expected = [
+        [2, 4],
+        [3, 4],
+        [2, 3],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+      ];
+      expect(process(actual)).to.eql(process(expected));
+    });
+    it('should solve test case', () => {
+      const actual = combine(1, 1);
+      const expected = [[1]];
       expect(process(actual)).to.eql(process(expected));
     });
   });
