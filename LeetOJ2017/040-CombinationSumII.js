70342bc2ba84b52b77f0026d14bdb6c164d5e0ef
anglee <ang.lee@gmail.com>
2017-02-20 08:29:50 -0500

    LeetOJ2017/040-CombinationSumII.js

---
+++ LeetOJ2017/040-CombinationSumII.js
@@ -0,0 +1,80 @@
+const compareAA = (A, B) => {
+  if (A.length !== B.length) {
+    return A.length - B.length;
+  }
+  for (let i = 0; i < A.length; ++i) {
+    if (A[i] !== B[i]) {
+      return A[i] - B[i];
+    }
+  }
+  return 0;
+};
+
+const eql = (A, B) => compareAA(A, B) === 0;
+
+const dedup = (AA) => {
+  AA.forEach(A => A.sort((a, b) => a - b));
+  AA.sort(compareAA);
+  const ret = [];
+  for (let i = 0; i < AA.length; ++i) {
+    if (ret.length === 0 || !eql(AA[i], ret[ret.length - 1])) {
+      ret.push(AA[i]);
+    }
+  }
+  return ret;
+};
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum0 = function(candidates, target) {
+  let buf = [[[]], ...Array(target).fill([])];
+
+  for (let c of candidates) {
+    const newBuf = [[[]], ...Array(target).fill([])];
+    for (let i = 1; i <= target; ++i) {
+      newBuf[i] = buf[i];
+      if (i - c >= 0 && buf[i - c].length > 0) {
+        newBuf[i] = newBuf[i].concat(
+          buf[i - c].map(r => r.concat(c))
+        );
+      }
+    }
+    buf = newBuf;
+  }
+
+  return dedup(buf[target]);
+};
+
+const without = (nums, num) => {
+  const ret = [];
+  for (let n of nums) {
+    if (n !== num) {
+      ret.push(n);
+    }
+  }
+  return ret;
+};
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  if (target === 0) {
+    return [[]];
+  }
+  if (target < 0 || candidates.length === 0) {
+    return [];
+  }
+  const [head, ...tail] = candidates;
+  return [
+    ...combinationSum(without(candidates, head), target),
+    ...combinationSum(tail, target - head).map(r => r.concat(head))
+  ];
+};
+
+export default combinationSum0;
---
+++ LeetOJ2017/040-CombinationSumII.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { sortAA } from '../lib/compare';
+import combinationSum2 from './040-CombinationSumII';
+
+describe('LeetOJ 040-CombinationSumII', () => {
+  describe('combinationSum2', () => {
+    it('should solve the given example', () => {
+      const candidates = [10, 1, 2, 7, 6, 1, 5];
+      const target = 8;
+      const expected = [
+        [1, 7],
+        [1, 2, 5],
+        [2, 6],
+        [1, 1, 6]
+      ];
+      const actual = combinationSum2(candidates, target);
+      expect(sortAA(actual))
+        .to.eql(sortAA(expected));
+    });
+  });
+});
