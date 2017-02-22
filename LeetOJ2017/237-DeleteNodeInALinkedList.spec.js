7fc02bedd49de6e80352186a6a2ae11758a76bd1
anglee <ang.lee@gmail.com>
2017-02-21 21:43:27 -0500

    Add LeetOJ2017/237-DeleteNodeInALinkedList.js

---
+++ LeetOJ2017/237-DeleteNodeInALinkedList.spec.js
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+import { fromArray, toArray, findNodeOfValue } from '../lib/LinkedList';
+import deleteNode from './237-DeleteNodeInALinkedList';
+
+describe('LeetOJ 237-DeleteNodeInALinkedList', () => {
+  describe('deleteNode', () => {
+    it('should solve the given example', () => {
+      const list = fromArray([1, 2, 3, 4]);
+      const node3 = findNodeOfValue(list, 3);
+      deleteNode(node3);
+      expect(toArray(list)).to.eql([1, 2, 4]);
+    });
+
+    // The OJ accepts the answer that doesn't take care of deleting the last node
+    // which seems to be impossible
+
+    // it('should be able to delete the last node', () => {
+    //   const list = fromArray([1,2,3,4]);
+    //   const node4 = findNodeOfValue(list, 4);
+    //   deleteNode(node4);
+    //   expect(toArray(list)).to.eql([1,2,3]);
+    // });
+  });
+});
