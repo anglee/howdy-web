71fbc7c65d7be4421e19188210daca3aad87ba41
anglee <ang.lee@gmail.com>
2017-03-06 07:17:16 -0500

    Refactor LeetOJ2017/023-MergeKSortedLists.js

--- LeetOJ2017/023-MergeKSortedLists.js
+++ LeetOJ2017/023-MergeKSortedLists.js
@@ -1,97 +1,5 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-
-export class LinkList {
-  constructor(array = []) {
-    this.head = null;
-    this.tail = null;
-
-    array.forEach((it) => {
-      const node = new ListNode(it);
-      this.add(node);
-    });
-  }
-
-  isEmpty() {
-    return this.head === null;
-  }
-
-  add(node) {
-    if (this.isEmpty()) {
-      this.head = this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-  }
-
-  getHead() {
-    return this.head;
-  }
-
-  toArray() { // debug only
-    const ret = [];
-    let node = this.head;
-    while (node) {
-      ret.push(node.val);
-      node = node.next;
-    }
-    return ret;
-  }
-}
-
-const binarySearch = (array, val) => {
-  // return the index of first element in array whose val is greater than val
-  // assumes array is sorted
-
-  if (array.length === 0) { return 0; }
-  if (array[array.length - 1].val < val) { return array.length; }
-
-  const binarySearchI = (i, j) => {
-    if (i === j) {
-      return i;
-    }
-
-    const m = Math.floor((i + j) / 2);
-
-    if (array[m].val < val) {
-      return binarySearchI(m + 1, j);
-    } else {
-      return binarySearchI(i, m);
-    }
-  };
-
-  return binarySearchI(0, array.length - 1);
-};
-
-
-class PriorityQueue {
-  constructor() {
-    this.queue = [];
-  }
-
-  add(node) {
-    // find the index of first element whose val is greater then node.val
-    const i = binarySearch(this.queue, node.val);
-    this.queue = [...this.queue.slice(0, i), node, ...this.queue.slice(i)];
-  }
-
-  pop() {
-    return this.queue.shift();
-  }
-
-  isEmpty() {
-    return this.queue.length === 0;
-  }
-
-  toArray() { // debug only
-    return this.queue.map(it => it.val);
-  }
-}
+import { LinkedList } from '../lib/LinkedList';
+import PriorityQueue from '../lib/PriorityQueue';
 
 /**
  * Definition for singly-linked list.
@@ -105,8 +13,8 @@ class PriorityQueue {
  * @return {ListListNode}
  */
 var mergeKLists = function(lists) {
-  const queue = new PriorityQueue();
-  const ret = new LinkList();
+  const queue = new PriorityQueue((a, b) => (a.val - b.val));
+  const ret = new LinkedList();
 
   lists.forEach((head) => {
     if (head) {
