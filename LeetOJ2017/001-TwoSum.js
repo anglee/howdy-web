1f005a2b56b0f0e963d94def2a9898d259c25029
anglee <ang.lee@gmail.com>
2017-03-20 08:38:13 -0400

    Update LeetOJ2017/001-TwoSum.js

--- LeetOJ2017/001-TwoSum.js
+++ LeetOJ2017/001-TwoSum.js
@@ -3,13 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
+const twoSum0 = function(nums, target) {
   for (var i = 0; i < nums.length - 1; i++) {
     for (var j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
+  }
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  const map = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i];
+    }
+    map.set(target - nums[i], i);
   }
 };
 
--- LeetOJ2017/001-TwoSum.spec.js
+++ LeetOJ2017/001-TwoSum.spec.js
@@ -1,12 +1,22 @@
 import { expect } from 'chai';
 import twoSum from './001-TwoSum';
 describe('LeetOJ 001-TwoSum', () => {
-  it('twoSum', () => {
-    const nums = [2, 7, 11, 15];
-    const target = 9;
-    const actual = twoSum(nums, target);
-    const expected = [0, 1];
-    expect(actual).to.eql(expected);
+  describe('twoSum', () => {
+    it('should solve the given example', () => {
+      const nums = [2, 7, 11, 15];
+      const target = 9;
+      const actual = twoSum(nums, target);
+      const expected = [0, 1];
+      expect(actual).to.eql(expected);
+    });
+    it('should solve OJ test case', () => {
+      const nums = [3,2,4];
+      const target = 6;
+      const actual = twoSum(nums, target);
+      const expected = [1, 2];
+      expect(actual).to.eql(expected);
+    });
+
   });
 });
 
