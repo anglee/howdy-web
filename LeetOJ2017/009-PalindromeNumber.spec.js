e802e93e9561c1d99fda3baf5e2d316eb662c7c1
anglee <ang.lee@gmail.com>
2017-03-22 22:41:10 -0400

    Update LeetOJ2017/009-PalindromeNumber.js

--- LeetOJ2017/009-PalindromeNumber.spec.js
+++ LeetOJ2017/009-PalindromeNumber.spec.js
@@ -3,11 +3,17 @@ import isPalindrome from './009-PalindromeNumber';
 
 describe('LeetOJ 009-PalindromeNumber', () => {
   describe('isPalindrome', () => {
-    it('should work with test case', () => {
+    it('should work with test case 1', () => {
       expect(isPalindrome(121)).to.be.true;
     });
-    it('should work with test case', () => {
+    it('should work with test case 2', () => {
       expect(isPalindrome(123)).to.be.false;
+    });
+    it('should work with test case 3', () => {
+      expect(isPalindrome(1210)).to.be.false;
+    });
+    it('should work with test case 4', () => {
+      expect(isPalindrome(-121)).to.be.false;
     });
   });
 });
