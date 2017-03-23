20bbe2bbcbf97f403a920adb9c41183c3b8d241d
anglee <ang.lee@gmail.com>
2017-03-23 07:08:42 -0400

    LeetOJ2017/010-RegularExpressionMatching.js

--- LeetOJ2017/010-RegularExpressionMatching.js
+++ LeetOJ2017/010-RegularExpressionMatching.js
@@ -3,6 +3,46 @@
  * @param {string} p
  * @return {boolean}
  */
+var isMatch0 = function(s, p) {
+  if (s.length === 0 && p.length === 0) {
+    return true;
+  }
+  if (p.length === 0) {
+    return false;
+  }
+  if (p.length >= 2 && p[1] === '*') {
+    if (p[0] === '.') {
+      return (
+        isMatch(s, p.substr(2)) || (
+          (s.length >= 1) && (
+            isMatch(s.substr(1), p.substr(2)) || isMatch(s.substr(1), p)
+          )
+        )
+      );
+    } else {
+      return (
+        isMatch(s, p.substr(2)) || (
+          (s.length >= 1 && s[0] === p[0]) && (
+            isMatch(s.substr(1), p.substr(2)) || isMatch(s.substr(1), p)
+          )
+        )
+      );
+    }
+  } else {
+    if (p[0] === '.') {
+      return s.length >= 1 && isMatch(s.substr(1), p.substr(1));
+    } else {
+      return s.length >= 1 && s[0] === p[0] && isMatch(s.substr(1), p.substr(1));
+    }
+  }
+};
+
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
 var isMatch = function(s, p) {
   if (p.length === 0) {
     return s.length === 0;
