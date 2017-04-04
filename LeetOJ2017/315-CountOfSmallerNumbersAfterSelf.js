1e2dfb8655038baaa8090b0ec02b2f5036d3dc7a
anglee <ang.lee@gmail.com>
2017-04-04 07:59:54 -0400

    LeetOJ2017/315-CountOfSmallerNumbersAfterSelf.js

--- LeetOJ2017/315-CountOfSmallerNumbersAfterSelf.js
+++ LeetOJ2017/315-CountOfSmallerNumbersAfterSelf.js
@@ -1,25 +1,21 @@
 const binarySearch = (array, node, compareFunc) => {
   // return the index of first element in array whose val is greater than val
   // assumes array is sorted
-
   if (array.length === 0) { return 0; }
-  if (compareFunc(array[array.length - 1], node) < 0) { return array.length; }
+  if (array[array.length - 1] <= val) { return array.length; }
 
-  const binarySearchI = (i, j) => {
-    if (i === j) {
-      return i;
-    }
+  let i = 0;
+  let j = array.length - 1;
 
+  while (i < j) {
     const m = Math.floor((i + j) / 2);
-
-    if (compareFunc(array[m], node) < 0) {
-      return binarySearchI(m + 1, j);
+    if (compareFunc(array[m], val) <= 0) {
+      i = m + 1;
     } else {
-      return binarySearchI(i, m);
+      j = m;
     }
-  };
-
-  return binarySearchI(0, array.length - 1);
+  }
+  return i;
 };
 
 class PriorityQueue {
@@ -31,11 +27,6 @@ class PriorityQueue {
   add(node) {
     // find the index of first element whose val is greater then node.val
     const i = binarySearch(this.queue, node, this.comparator);
-    // this.queue = [
-    //   ...this.queue.slice(0, i),
-    //   node,
-    //   ...this.queue.slice(i)
-    // ];
     this.queue.splice(i, 0, node);
   }
 
@@ -49,10 +40,6 @@ class PriorityQueue {
 
   isEmpty() {
     return this.queue.length === 0;
-  }
-
-  toArray() { // debug only
-    return this.queue.map(it => it.val);
   }
 }
 
