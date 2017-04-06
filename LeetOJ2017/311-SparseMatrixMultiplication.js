641be977f869836d080a7ca578d2336bac68da9b
anglee <ang.lee@gmail.com>
2017-04-06 07:02:54 -0400

    Improve LeetOJ2017/311-SparseMatrixMultiplication.js

--- LeetOJ2017/311-SparseMatrixMultiplication.js
+++ LeetOJ2017/311-SparseMatrixMultiplication.js
@@ -1,4 +1,9 @@
-var multiply = function(A, B) {
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+var multiply0 = function(A, B) {
   const AB = [];
   for (let rowA = 0; rowA < A.length; rowA++) {
     const rowInAB = [];
@@ -10,6 +15,28 @@ var multiply = function(A, B) {
       rowInAB.push(val);
     }
     AB.push(rowInAB);
+  }
+
+  return AB;
+};
+
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+var multiply = function(A, B) {
+  const AB = Array(A.length).fill().map(() => Array(B[0].length).fill(0));
+  for (let rowA = 0; rowA < A.length; rowA++) {
+    for (let i = 0; i < A[0].length; i++) {
+      if (A[rowA][i] !== 0) {
+        for (let colB = 0; colB < B[0].length; colB++) {
+          if (B[i][colB] !== 0) {
+            AB[rowA][colB] += A[rowA][i] * B[i][colB];
+          }
+        }
+      }
+    }
   }
 
   return AB;
