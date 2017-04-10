0782cb3a7e96fa65e169bc41d99dd48f11ea3375
anglee <ang.lee@gmail.com>
2017-04-10 07:47:45 -0400

    Add alternative solution for LeetOJ2017/285-InorderSuccessorInBST.js

--- LeetOJ2017/285-InorderSuccessorInBST.js
+++ LeetOJ2017/285-InorderSuccessorInBST.js
@@ -12,7 +12,7 @@ import _ from 'lodash';
  * @param {TreeNode} p
  * @return {TreeNode}
  */
-var inorderSuccessor = function(root, p) {
+var inorderSuccessor0 = function(root, p) {
 
   let lastNode = null;
 
@@ -44,4 +44,65 @@ var inorderSuccessor = function(root, p) {
   return traverse(root);
 
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+  const findLeftMostLeaf = (node) => {
+    let current = node;
+    while (current && current.left) {
+      current = current.left;
+    }
+    return current;
+  };
+
+  const memoize = (f) => {
+    const map = new Map();
+    return (node) => {
+      if (map.has(node)) {
+        return map.get(node);
+      }
+      const ret = f(node);
+      map.set(node, ret);
+      return ret;
+    }
+  };
+
+  const findParent = memoize((node) => {
+    const q = [root];
+    while (q.length > 0) {
+      const current = q.shift();
+      if (current === null) {
+        continue;
+      }
+      if (current.left === node || current.right === node) {
+        return current;
+      }
+      q.push(current.left, current.right);
+    }
+    return null;
+  });
+
+  if (p.right !== null) {
+    return findLeftMostLeaf(p.right);
+  } else {
+    let node = p;
+    while (node) {
+      let parent = findParent(node);
+      if (parent === null) {
+        return null;
+      }
+      if (node === parent.left) {
+        return parent;
+      } else {
+        node = parent;
+      }
+    }
+    return null;
+  }
+};
+
 export default inorderSuccessor;
--- LeetOJ2017/285-InorderSuccessorInBST.spec.js
+++ LeetOJ2017/285-InorderSuccessorInBST.spec.js
@@ -15,5 +15,21 @@ describe('LeetOJ 285-InorderSuccessorInBST', () => {
       const node = inorderSuccessor(root, root);
       expect(node).to.equal(root.right);
     });
+    it('solve test case 1', () => {
+      const root = treeDeserializer([1]);
+      const node = inorderSuccessor(root, root);
+      expect(node).to.equal(null);
+    });
+    it('solve test case 2', () => {
+      const root = treeDeserializer([2, 1]);
+      const node = inorderSuccessor(root, root);
+      expect(node).to.equal(root.right);
+    });
+
+    it('solve test case 3', () => {
+      const root = treeDeserializer([2, '#', 3]);
+      const node = inorderSuccessor(root, root.right);
+      expect(node).to.equal(null);
+    });
   });
 });
