1a8d57ee466ab8d11b79572cd57e68f609cfb6f5
anglee <ang.lee@gmail.com>
2017-04-17 08:21:19 -0400

    Redo LeetOJ2017/079-WordSearch.js

--- LeetOJ2017/079-WordSearch.js
+++ LeetOJ2017/079-WordSearch.js
@@ -3,7 +3,7 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
+var exist0 = function(board, word) {
   const height = board.length;
   const width = height > 0 ? board[0].length : 0;
 
@@ -48,5 +48,47 @@ var exist = function(board, word) {
   }
   return false;
 };
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+  const h = board.length;
+  if (h === 0) { return false; }
+  const w = board[0].length;
+  if (w === 0) { return false; }
+  for (let y = 0; y < h; ++y) {
+    for (let x = 0; x < w; ++x) {
+      const isUsed = Array(h).fill().map(() => Array(w).fill(false));
+      const search = (x, y, i) => {
+        if (x < 0 || x >= w || y < 0 || y >= h || isUsed[y][x]) {
+          return false;
+        }
+        if (board[y][x] !== word[i]) {
+          return false;
+        }
+        if (i === word.length - 1) {
+          return true;
+        }
+        isUsed[y][x] = true;
+        return (
+          search(x - 1, y, i + 1) ||
+          search(x + 1, y, i + 1) ||
+          search(x, y - 1, i + 1) ||
+          search(x, y + 1, i + 1)
+        );
+      };
+      const found = search(x, y, 0);
+      if (found) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+export default exist;
 
 export default exist;
