a913f6f9755f16fb7a42c27c1823a32f9bb0a3ad
anglee <ang.lee@gmail.com>
2017-04-22 16:02:23 -0400

    LeetOJ2017/418-SentenceScreenFitting.js

--- LeetOJ2017/418-SentenceScreenFitting.js
+++ LeetOJ2017/418-SentenceScreenFitting.js
@@ -66,7 +66,7 @@ var wordsTyping0 = function(sentence, rows, cols) {
  * @param {number} cols
  * @return {number}
  */
-var wordsTyping = function(sentence, rows, cols) {
+var wordsTyping1 = function(sentence, rows, cols) {
   for (let w of sentence) {
     // if the word is wider than cols, there is no way the sentence can fit
     if (w.length > cols) {
@@ -124,6 +124,136 @@ var wordsTyping = function(sentence, rows, cols) {
     }
     ret++;
   }
+};
+
+/**
+ * @param {string[]} sentence
+ * @param {number} rows
+ * @param {number} cols
+ * @return {number}
+ */
+var wordsTyping2 = function(sentence, rows, cols) {
+  for (let w of sentence) {
+    // if the word is wider than cols, there is no way the sentence can fit
+    if (w.length > cols) {
+      return 0;
+    }
+  }
+
+  const chars = sentence.join(' ') + ' ';
+  const memoize = (f) => {
+    const map = new Map();
+    return (pos) => {
+      if (map.has(pos)) {
+        return map.get(pos);
+      }
+      const ret = f(pos);
+      map.set(pos, ret);
+      return ret;
+    }
+  };
+
+  // consider the case:
+  // const rows = 3;
+  // const cols = 6;
+  // const sentence = ["a", "bcd", "e"];
+  // chars = ['a', '_', 'b', 'c', 'd', '_', 'e', '_'];
+  //                           0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7|0 1 2 3 4 5 6 7
+  //                           a _ b c d _ e _|a _ b c d _ e _|a _ b c d _ e _
+  // row 0, start = 0, end = 5 * * * * * *
+  // row 1, start = 6, end = 1             * * * * x x
+  // row 2, start = 2, end = 7                     * * * * * *
+
+  const processRow = memoize((startPosInSentence) => {
+    let end = startPosInSentence + cols - 1;
+    if (chars[(end + 1) % chars.length] === ' ') {
+      end = end + 1;
+    } else {
+      while (chars[end % chars.length] !== ' ') {
+        end--;
+      }
+    }
+    return {
+      nextStart: (end + 1) % chars.length,
+      count: Math.floor((end + 1) / chars.length),
+    };
+  });
+
+  let start = 0;
+  let ret = 0;
+  for (let i = 0; i < rows; ++i) {
+    const { nextStart, count } = processRow(start);
+    start = nextStart;
+    ret += count;
+  }
+  return ret;
+};
+
+/**
+ * @param {string[]} sentence
+ * @param {number} rows
+ * @param {number} cols
+ * @return {number}
+ */
+var wordsTyping3 = function(sentence, rows, cols) {
+  let start = 0;
+  const chars = sentence.join(' ') + ' ';
+  const processRow = (start) => {
+    let nextStart = start + cols;
+    if (chars[nextStart % chars.length] === ' ') {
+      nextStart++;
+    } else {
+      while (nextStart > 0 && chars[(nextStart - 1)  % chars.length] !== ' ') {
+        nextStart--;
+      }
+    }
+    return nextStart - start;
+  };
+  for (let i = 0; i < rows; ++i) {
+    const offset = processRow(start);
+    start += offset;
+  }
+  return Math.floor(start / chars.length);
+};
+
+
+/**
+ * @param {string[]} sentence
+ * @param {number} rows
+ * @param {number} cols
+ * @return {number}
+ */
+var wordsTyping = function(sentence, rows, cols) {
+  let start = 0;
+  const chars = sentence.join(' ') + ' ';
+  const memoize = (f) => {
+    const map = new Map();
+    return (input) => {
+      if (map.has(input)) {
+        return map.get(input);
+      }
+      const ret = f(input);
+      map.set(input, ret);
+      return ret;
+    }
+  };
+
+  const getOffset = memoize((start) => {
+    let nextStart = start + cols;
+    if (chars[nextStart % chars.length] === ' ') {
+      nextStart++;
+    } else {
+      while (nextStart > 0 && chars[(nextStart - 1)  % chars.length] !== ' ') {
+        nextStart--;
+      }
+    }
+    return nextStart - start;
+  });
+
+  for (let i = 0; i < rows; ++i) {
+    start += getOffset(start % chars.length);
+  }
+  return Math.floor(start / chars.length);
 };
 
 export default wordsTyping;
