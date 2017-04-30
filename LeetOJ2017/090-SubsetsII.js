f7f391ff8bb0e0d88ddb4a1135b3ab574a19f68b
anglee <ang.lee@gmail.com>
2017-04-30 10:09:39 -0400

    Improve LeetOJ2017/090-SubsetsII.js

--- LeetOJ2017/090-SubsetsII.js
+++ LeetOJ2017/090-SubsetsII.js
@@ -1,35 +1,24 @@
-
-
-const subset = (countMap) => {
-  if (countMap.size === 0) {
-    return [[]];
-  }
-  const [[element, count]] = Array.from(countMap.entries()); // take the 1st entry
-  countMap.delete(element);
-  const ss = subset(countMap);
-  const ret = [];
-  for (let c = 0; c <= count; ++c) {
-    ss.forEach((it) => {
-      ret.push([...Array(c).fill(element), ...it]);
-    });
-  }
-  return ret;
-};
-
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-  const countMap = new Map();
-  nums.forEach((num) => {
-    if (!countMap.has(num)) {
-      countMap.set(num, 1);
-    } else {
-      countMap.set(num, countMap.get(num) + 1)
+  const countMap = nums.reduce((map, num) => {
+    map.set(num, map.has(num) ? map.get(num) + 1 : 1);
+    return map;
+  }, new Map());
+
+  let ret = [[]];
+  for (const [num, count] of countMap) {
+    const next = [];
+    for (var c = 0; c <= count; ++c) {
+      ret.forEach((it) => {
+        next.push([...it, ...Array(c).fill(num)]);
+      });
     }
-  });
-  return subset(countMap);
+    ret = next;
+  }
+  return ret;
 };
 
 export default subsetsWithDup;
--- LeetOJ2017/090-SubsetsII.spec.js
+++ LeetOJ2017/090-SubsetsII.spec.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
+import { sortAA } from '../lib/compare';
 import subsetsWithDup from './090-SubsetsII';
 
 describe('LeetOJ 090-SubsetsII', () => {
-
+  
   describe('subsetsWithDup', () => {
     it('should solve the given example', () => {
       const nums = [1, 2, 2];
@@ -14,8 +15,8 @@ describe('LeetOJ 090-SubsetsII', () => {
         [1, 2],
         []
       ];
-      expect(new Set(subsetsWithDup(nums)))
-        .to.eql(new Set(expected));
+      expect(sortAA(subsetsWithDup(nums)))
+        .to.eql(sortAA(expected));
     });
   });
 });
