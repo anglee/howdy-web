dd55dda41482018f290a9f3474a496d823a7f1da
anglee <ang.lee@gmail.com>
2017-05-01 08:54:59 -0400

    Add DP solution for LeetOJ2017/044-WildcardMatching.js

--- LeetOJ2017/044-WildcardMatching.js
+++ LeetOJ2017/044-WildcardMatching.js
@@ -3,7 +3,7 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
+var isMatch0 = function(s, p) { // recursive + memoize
 
   const memoize = (originalFunc) => {
     const map = new Map();
@@ -51,6 +51,139 @@ var isMatch = function(s, p) {
   });
 
   return isMatchI(s, p);
+};
+
+//------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch1 = function(s, p) { // recursive + memoize, minor clean up from isMatch0
+  const memoize = (f) => {
+    const map = new Map();
+    return (s, p) => {
+      const key = `${s.length}_${p.length}`; // <------ cool!
+      if (map.has(key)) {
+        return map.get(key);
+      }
+      const ret = f(s, p);
+      map.set(key, ret);
+      return ret;
+    };
+  };
+
+  var helper = memoize(function(s, p) {
+
+    if (s.length === 0 && p.length === 0) {
+      return true;
+    }
+
+    if (p.length > 0) {
+      if (p[0] === '*') {
+        if (s.length > 0) {
+          return (
+            helper(s, p.substr(1)) ||
+            helper(s.substr(1), p)
+          );
+        } else {
+          return helper(s, p.substr(1));
+        }
+      } else if (p[0] === '?') {
+        if (s.length > 0) {
+          return helper(s.substr(1), p.substr(1));
+        }
+        return false;
+      } else {
+        if (s.length > 0 && s[0] === p[0]) {
+          return helper(s.substr(1), p.substr(1));
+        }
+        return false;
+      }
+    }
+    return false;
+  });
+
+  return helper(s, p);
+};
+
+//------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch2 = function(s, p) { // DP, dynamic programming, space: O(s.length * p.length)
+
+  const AA = Array(s.length + 1).fill().map(
+    () => Array(p.length + 1).fill(false)
+  );
+
+  AA[0][0] = true;
+  for (let i = 1; i <= p.length; ++i) {
+    const ch_p = p[i - 1];
+    if (ch_p === '*') {
+      AA[0][i] = AA[0][i - 1];
+    }
+  }
+
+  for (let j = 1; j <= s.length; ++j) {
+    const ch_s = s[j - 1];
+    for (let i = 1; i <= p.length; ++i) {
+      const ch_p = p[i - 1];
+      if (ch_p === '*') {
+        AA[j][i] = AA[j][i - 1] || AA[j- 1][i];
+      } else {
+        if (ch_p === '?' || ch_p === ch_s) {
+          AA[j][i] = AA[j- 1][i - 1];
+        }
+      }
+    }
+  }
+  return AA[s.length][p.length];
+};
+
+//------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) { // DP, dynamic programming, space: O(p.length)
+
+  let A = Array(p.length + 1).fill(false);
+
+  A[0] = true;
+  for (let i = 1; i <= p.length; ++i) {
+    const ch_p = p[i - 1];
+    if (ch_p === '*') {
+      A[i] = A[i - 1];
+    }
+  }
+
+  for (let j = 1; j <= s.length; ++j) {
+    const ch_s = s[j - 1];
+    const nextA = Array(p.length + 1).fill(false);
+    for (let i = 1; i <= p.length; ++i) {
+      const ch_p = p[i - 1];
+      if (ch_p === '*') {
+        nextA[i] = nextA[i - 1] || A[i];
+      } else {
+        if (ch_p === '?' || ch_p === ch_s) {
+          nextA[i] = A[i - 1];
+        }
+      }
+    }
+    A = nextA;
+  }
+
+  return A[p.length];
 };
 
 export default isMatch;
