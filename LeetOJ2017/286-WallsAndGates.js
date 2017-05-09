0af5fde913a732607033c40b3061ef2847a97b0c
anglee <ang.lee@gmail.com>
2017-05-09 07:56:46 -0400

    Add alternative to LeetOJ2017/286-WallsAndGates.js

--- LeetOJ2017/286-WallsAndGates.js
+++ LeetOJ2017/286-WallsAndGates.js
@@ -1,8 +1,45 @@
+const GATE = 0;
+const WALL = -1;
+
 /**
  * @param {number[][]} rooms
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
-var wallsAndGates = function(rooms) {
+var wallsAndGates0 = function(rooms) { // DFS
+  const h = rooms.length;
+  if (h === 0) { return; }
+  const w = rooms[0].length;
+
+  const dfs = (rooms, x, y, val) => {
+    if (x < 0 || y < 0 || x >= w || y >= h || rooms[y][x] === WALL) {
+      return;
+    }
+    if (rooms[y][x] < val) {
+      return;
+    }
+    rooms[y][x] = val;
+    dfs(rooms, x - 1, y, val + 1);
+    dfs(rooms, x + 1, y, val + 1);
+    dfs(rooms, x, y - 1, val + 1);
+    dfs(rooms, x, y + 1, val + 1);
+  };
+
+
+  for (let y = 0; y < h; ++y) {
+    for (let x = 0; x < w; ++x) {
+      if (rooms[y][x] === GATE) {
+        dfs(rooms, x, y, 0);
+      }
+    }
+  }
+};
+
+
+/**
+ * @param {number[][]} rooms
+ * @return {void} Do not return anything, modify rooms in-place instead.
+ */
+var wallsAndGates = function(rooms) { // BFS
 
   const h = rooms.length;
   if (h === 0) { return; }
@@ -16,12 +53,12 @@ var wallsAndGates = function(rooms) {
       if (
         x < 0 || x >= w ||
         y < 0 || y >= h ||
-        rooms[y][x] === -1
+        rooms[y][x] === WALL
       ) {
         continue;
       }
 
-      if (rooms[y][x] > to) {
+      if (rooms[y][x] >= to) {
         // console.log(`rooms[${y}][${x}] from:${rooms[y][x]} to:${to}`);
         rooms[y][x] = to;
         q.push([ x - 1, y, to + 1 ]);
@@ -34,11 +71,8 @@ var wallsAndGates = function(rooms) {
 
   for (let y = 0; y < h; ++y) {
     for (let x = 0; x < w; ++x) {
-      if (rooms[y][x] === 0) {
-        mark(x - 1, y, 1);
-        mark(x + 1, y, 1);
-        mark(x, y - 1, 1);
-        mark(x, y + 1, 1);
+      if (rooms[y][x] === GATE) {
+        mark(x, y, 0);
       }
     }
   }
