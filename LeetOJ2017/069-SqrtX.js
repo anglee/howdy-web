1e62134d95ed48b231950929ab289e2174aca2c9
anglee <ang.lee@gmail.com>
2017-05-10 07:06:32 -0400

    Improve LeetOJ2017/069-SqrtX.js

--- LeetOJ2017/069-SqrtX.js
+++ LeetOJ2017/069-SqrtX.js
@@ -38,26 +38,20 @@ var mySqrt = function(x) {
   if (x < 0) {
     return Number.NaN;
   }
-  if (x === 0 || x === 1) {
-    return x;
-  }
-  let p = x > 1 ? 1 : 0;
-  let q = x > 1 ? x : 0;
-  while (p !== q) {
-    const m = Math.floor((p + q) / 2);
-    // console.log(p, q, m);
-    if (m * m < x) {
-      if (p === m) {
-        return m;
-      }
-      p = m;
-    } else if (m * m > x) {
-      if (q === m) {
-        return m;
-      }
-      q = m;
-    } else { // m * m == x;
-      return m;
+  if (x <= 1) { return x; }
+  let i = 0;
+  let j = x;
+  while (i < j) {
+    const m = Math.floor((i + j) / 2);
+    if (m === i) { return m; }
+
+    const mSq = m * m;
+    if (mSq === x) { return m; }
+
+    if (mSq < x) {
+      i = m;
+    } else {
+      j = m;
     }
   }
 };
--- LeetOJ2017/069-SqrtX.spec.js
+++ LeetOJ2017/069-SqrtX.spec.js
@@ -21,29 +21,32 @@ describe('LeetOJ 069-SqrtX', () => {
     });
   });
   describe('mySqrt', () => {
-    it('should solve test case 1', () => {
-      expect(mySqrt(4)).to.equal(2);
-    });
-    it('should solve test case 2', () => {
+    it('should solve test case 0', () => {
       expect(mySqrt(0)).to.equal(0);
     });
-    it('should solve test case 3', () => {
+    it('should solve test case 1', () => {
       expect(mySqrt(1)).to.equal(1);
     });
-    it('should solve test case 4', () => {
+    it('should solve test case 2', () => {
       expect(mySqrt(2)).to.equal(1);
     });
-    it('should solve test case 5', () => {
+    it('should solve test case 3', () => {
       expect(mySqrt(3)).to.equal(1);
     });
-    it('should solve test case 6', () => {
+    it('should solve test case 4', () => {
+      expect(mySqrt(4)).to.equal(2);
+    });
+    it('should solve test case 100', () => {
       expect(mySqrt(100)).to.equal(10);
     });
-    it('should solve test case 7', () => {
+    it('should solve test case 99', () => {
       expect(mySqrt(99)).to.equal(9);
     });
-    it('should solve test case 8', () => {
+    it('should solve test case 101', () => {
       expect(mySqrt(101)).to.equal(10);
+    });
+    it('should solve test case -1', () => {
+      expect(mySqrt(-1)).to.eql(Number.NaN);
     });
   });
 });
