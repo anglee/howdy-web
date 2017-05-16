0f80abcca3cee3fda35e0df32cd280e6af4c7b93
anglee <ang.lee@gmail.com>
2017-05-16 07:27:54 -0400

    Improve LeetOJ2017/274-HIndex.js

--- LeetOJ2017/274-HIndex.js
+++ LeetOJ2017/274-HIndex.js
@@ -2,19 +2,32 @@
  * @param {number[]} citations
  * @return {number}
  */
-var hIndex = function(citations) {
-  citations.sort((a, b) => (b - a));
-  // case h === citations.length;
-  let h = citations.length;
-  if (citations[h - 1] >= h) {
-    return h;
-  }
-  for (let h = citations.length - 1; h >= 1; --h) {
-    if (citations[h - 1] >=h && citations[h] <= h) {
-      return h;
+var hIndex0 = function (citations) { // time: O(n ^ 2)
+  for (let h = citations.length; h > 0; --h) {
+    let count = 0;
+    for (let citation of citations) {
+      if (citation >= h) {
+        ++count;
+        if (count >= h) {
+          return h;
+        }
+      }
     }
   }
   return 0;
 };
 
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) { // time: O(n log n)
+  citations.sort((a, b) => b - a);
+  for (let h = citations.length; h > 0; --h) {
+    if (citations[h - 1] >= h) {
+      return h
+    }
+  }
+  return 0;
+};
 export default hIndex;
