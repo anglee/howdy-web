74e4404b42dae685f77dc0b1a4afe536f2228c03
anglee <ang.lee@gmail.com>
2017-05-17 06:03:33 -0400

    Alternative LeetOJ2017/461-HammingDistance.js

--- LeetOJ2017/461-HammingDistance.js
+++ LeetOJ2017/461-HammingDistance.js
@@ -3,7 +3,7 @@
  * @param {number} y
  * @return {number}
  */
-var hammingDistance = function(x, y) {
+var hammingDistance0 = function(x, y) {
   const binaryX = x.toString(2);
   const binaryY = y.toString(2);
   let ret = 0;
@@ -14,6 +14,37 @@ var hammingDistance = function(x, y) {
       ++ret;
     }
   }
+  return ret;
+};
+
+//------------------------------------------------------------------------------------------
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+
+  let ix = x;
+  let iy = y;
+
+  let ret = 0;
+  while (ix > 0 && iy > 0) {
+    if (ix % 2 !== iy % 2) { ret += 1; }
+    ix = Math.floor(ix / 2);
+    iy = Math.floor(iy / 2);
+  }
+
+  while (ix > 0) {
+    if (ix % 2 !== 0) { ret += 1; }
+    ix = Math.floor(ix / 2);
+  }
+  while (iy > 0) {
+    if (iy % 2 !== 0) { ret += 1; }
+    iy = Math.floor(iy / 2);
+  }
+
   return ret;
 };
 
