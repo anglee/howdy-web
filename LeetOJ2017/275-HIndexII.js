50f182bbf3d2f2f06b7abd1afd968d1956b1e9ba
anglee <ang.lee@gmail.com>
2017-05-21 11:03:43 -0400

    Alternative to LeetOJ2017/275-HIndexII.js

--- LeetOJ2017/275-HIndexII.js
+++ LeetOJ2017/275-HIndexII.js
@@ -2,6 +2,21 @@
  * @param {number[]} citations
  * @return {number}
  */
+var hIndex0 = function(citations) {
+  let h = citations.length;
+  while (h > 0 && citations[citations.length - h] < h) {
+    --h;
+  }
+  return h;
+};
+
+//------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
 var hIndex = function(citations) {
   for (let h = citations.length; h >= 1; --h) {
     if (citations[citations.length - h] >= h) {
