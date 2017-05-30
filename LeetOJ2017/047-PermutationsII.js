b51062ff80a4d68b1ae709f4710499dedce05ca9
anglee <ang.lee@gmail.com>
2017-05-30 08:11:41 -0400

    Alternative to LeetOJ2017/047-PermutationsII.js

--- LeetOJ2017/047-PermutationsII.js
+++ LeetOJ2017/047-PermutationsII.js
@@ -42,6 +42,8 @@ var permuteUnique0 = function(nums) {
   return permute0(countMap);
 };
 
+//--------------------------------------------------------------------------------------------------
+
 
 /**
  * @param {number[]} nums
@@ -65,6 +67,59 @@ var permuteUnique = function (nums) {
   };
   doPermute([], countMap);
   return ret;
+};
+
+
+//--------------------------------------------------------------------------------------------------
+
+const generateHashKey = (countMap) => {
+  return Array.from(countMap.keys()).sort().map(key => `${key}.${countMap.get(key)}`).join('_') || '';
+};
+
+const memoize = (f) => {
+  const map = new Map();
+  return (countMap) => {
+    const hashKey = generateHashKey(countMap);
+    if (map.has(hashKey)) {
+      return map.get(hashKey);
+    }
+    const ret = f(countMap);
+    map.set(hashKey, ret);
+    return ret;
+  };
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  const doPermuteUnique = memoize((countMap) => {
+    if (countMap.size === 0) {
+      return [[]];
+    }
+
+    const ret = [];
+    // note that here we iterate over an Array.from instead of just countMap.entries(), because
+    // we touch the map instance in the loop (delete, set), if a key is deleted then add back,
+    // it is considered a different entry and thus will add a iteration to the looping
+    for (let [num, count] of Array.from(countMap.entries())) {
+      if (count === 1) {
+        countMap.delete(num);
+      } else {
+        countMap.set(num, count - 1);
+      }
+
+      ret.push(
+        ...doPermuteUnique(countMap).map(it => [num, ...it])
+      );
+      countMap.set(num, count);
+    }
+    return ret;
+  });
+
+  const countMap = nums.reduce((map, num) => map.set(num, map.get(num) + 1 || 1), new Map());
+  return doPermuteUnique(countMap);
 };
 
 export default permuteUnique;
