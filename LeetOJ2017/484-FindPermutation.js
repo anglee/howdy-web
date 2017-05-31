cd3fe1ba605405830e44f45a7749c4455845b1d0
anglee <ang.lee@gmail.com>
2017-05-31 08:25:18 -0400

    Alternative LeetOJ2017/484-FindPermutation.js

--- LeetOJ2017/484-FindPermutation.js
+++ LeetOJ2017/484-FindPermutation.js
@@ -1,3 +1,55 @@
+const countDs = (sig, i) => {
+  let count = 0;
+  let x = i + 1;
+  while (sig[x] === 'D') {
+    ++count;
+    ++x;
+  }
+  return count;
+};
+
+const fillRet = (ret, i, count) => {
+  for (let x = 0; x <= count; ++ x) {
+    ret[i + count - x] = i + x + 1;
+  }
+  return ret;
+};
+
+// iterate from left to right, when coming across an 'I',
+// count how many 'D's are following the 'I' (at i),
+// use the information of i, and count of D's to fill the range [i, i + count + 1] in the
+// result array(ret) with decreasing numbers starting with i + 1,
+// and then move to the next 'I'
+// and for convenience, if input s starts with 'D', prepend an 'I'
+// For example, s = DDDIIDD
+// sig = IDDDIIDD <- s prepended with I
+// (IDDD)(I)(IDD)
+// at the 1st I (i = 0, count of following D's = 3),
+// fill ret with [4,3,2,1]
+// at the 2nd I, if (i = 4, count of following D's = 0)
+// fill ret with [_,_,_,_,5]
+// at the 3rd I, if (i = 4, count of following D's = 2)
+// fill ret with [_,_,_,_,_,8,7,6]
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var findPermutation0 = function(s) {
+  const sig = ['I', ...s];
+  let i = 0;
+  let ret = Array(sig.length);
+  while (i < sig.length) {
+    const count = countDs(sig, i);
+    fillRet(ret, i, count);
+    i += count + 1;
+  }
+  return ret;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+
 /**
  * @param {string} s
  * @return {number[]}
--- LeetOJ2017/484-FindPermutation.spec.js
+++ LeetOJ2017/484-FindPermutation.spec.js
@@ -13,5 +13,65 @@ describe('LeetOJ 484-Find Permutation', () => {
       const expected = [2, 1, 3];
       expect(findPermutation(input)).to.eql(expected);
     });
+    it('should solve test case 1', () => {
+      const input = 'I';
+      const expected = [1, 2];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 2', () => {
+      const input = 'D';
+      const expected = [2, 1];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 3', () => {
+      const input = 'ID';
+      const expected = [1, 3, 2];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 4', () => {
+      const input = 'DI';
+      const expected = [2, 1, 3];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 5', () => {
+      const input = 'II';
+      const expected = [1, 2, 3];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 6', () => {
+      const input = 'DD';
+      const expected = [3, 2, 1];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 7', () => {
+      const input = 'IID';
+      const expected = [1,2,4,3];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 8', () => {
+      const input = 'IDI';
+      const expected = [1,3,2,4];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 9', () => {
+      const input = 'IDD';
+      const expected = [1,4,3,2];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 10', () => {
+      const input = 'IIDD';
+      const expected = [1,2,5,4,3];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 11', () => {
+      const input = 'IDID';
+      const expected = [1,3,2,5,4];
+      expect(findPermutation(input)).to.eql(expected);
+    });
+    it('should solve test case 12', () => {
+      const input = 'IIIDDIIDD';
+      const expected = [1,2,3,6,5,4,7,10,9,8];
+      expect(findPermutation(input)).to.eql(expected);
+    });
   });
 });
