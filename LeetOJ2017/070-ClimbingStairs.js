6dc73d15b28df5a32a92e5d871aedd619e0909d6
anglee <ang.lee@gmail.com>
2017-06-12 08:46:32 -0400

    Alt sol LeetOJ2017/070-ClimbingStairs.js

--- LeetOJ2017/070-ClimbingStairs.js
+++ LeetOJ2017/070-ClimbingStairs.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+var climbStairs0 = function(n) {
   const step = Array(n + 1).fill(0);
   step[0] = 1;
   step[1] = 1;
@@ -10,6 +10,25 @@ var climbStairs = function(n) {
     step[i] = step[i - 1] + step[i - 2];
   }
   return step[n];
+};
+
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  let tmp1 = 1;
+  let tmp2 = 1;
+  let i = 2;
+  while (i <= n) {
+    const sum = tmp1 + tmp2;
+    tmp1 = tmp2;
+    tmp2 = sum;
+    ++i;
+  }
+  return tmp2;
 };
 
 export default climbStairs;
