7f0ec52c5bd5b2aa4f72abb49d05342dca474e45
anglee <ang.lee@gmail.com>
2017-06-17 10:13:44 -0400

    Alternative LeetOJ2017/287-FindTheDuplicateNumber.js

--- LeetOJ2017/287-FindTheDuplicateNumber.js
+++ LeetOJ2017/287-FindTheDuplicateNumber.js
@@ -15,7 +15,7 @@ const isMoreThanOnce = (A, x) => {
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate0 = function(nums) { // time O(n^2)
+var findDuplicate0 = function(nums) { // brute-force, time O(n^2)
   const n = nums.length - 1;
 
   for (let i = 1; i <= n; ++i) {
@@ -25,12 +25,51 @@ var findDuplicate0 = function(nums) { // time O(n^2)
   }
 };
 
-// =====================================================
+//--------------------------------------------------------------------------------------------------
 
-const countSmallerThanOrEqualToMid = (A, mid) => {
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate1 = function(nums) { // brute-force, time O(n^2)
+  for (let i = 0; i < nums.length - 1; ++i) {
+    for (let j = i + 1; i < nums.length; ++i) {
+      if (nums[j] == nums[i]) {
+        return nums[j]
+      }
+    }
+  }
+};
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate2 = function(nums) { // modify the original (failed the assumption that nums is read-only
+  let dup = null;
+  for (let i = 0; i < nums.length; ++i) {
+    const num = nums[i];
+    if (nums[num - 1] > 0) {
+      nums[num - 1] *= -1;
+    } else {
+      dup = num;
+    }
+  }
+  // restore nums
+  for (let i = 0; i < nums.length; ++i) {
+    nums[i] = Math.abs(nums[i]);
+  }
+  return dup;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+
+const countSmallerThanOrEqualToTarget = (A, target) => {
   let count = 0;
   for (let i = 0; i < A.length; ++i) {
-    if (A[i] <= mid) {
+    if (A[i] <= target) {
       ++count;
     }
   }
@@ -48,7 +87,7 @@ var findDuplicate = function(nums) { // time O(n log n)
   let j = n;
   while (i < j) {
     let mid = Math.floor((i + j) / 2);
-    const count = countSmallerThanOrEqualToMid(nums, mid);
+    const count = countSmallerThanOrEqualToTarget(nums, mid);
     // console.log('mid', mid, 'count', count);
     if (count > mid) {
       j = mid;
