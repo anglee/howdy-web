d70fbdf895f900620965853459452f64edc49e7b
anglee <ang.lee@gmail.com>
2017-06-18 12:33:50 -0400

    Alternative LeetOJ2017/265-PaintHouseII.js

--- LeetOJ2017/265-PaintHouseII.js
+++ LeetOJ2017/265-PaintHouseII.js
@@ -2,11 +2,82 @@
  * @param {number[][]} costs
  * @return {number}
  */
+var minCostII0 = function(costs) { // by recursion + memoization
+  if (costs.length === 0 || costs[0].length === 0) {
+    return 0;
+  }
+
+  const houseCount = costs.length;
+  const colorCount = costs[0].length;
+  const colors = Array(colorCount).fill().map((it, i) => i);
+
+  const memoize = f => {
+    const map = new Map();
+    return (h, c) => {
+      const key = `${h}_${c}`;
+      if (map.has(key)) {
+        return map.get(key);
+      }
+      const ret = f(h, c);
+      map.set(key, ret);
+      return ret;
+    }
+  };
+  const doPaint = memoize((h, c0) => {
+    if (h === houseCount - 1) { // last house
+      return costs[h][c0];
+    }
+    return costs[h][c0] + Math.min(
+      ...colors.filter(c => c !== c0).map(c => doPaint(h + 1, c))
+    );
+  });
+
+  return Math.min(
+    ...colors.map(c => doPaint(0, c))
+  );
+};
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var minCostII1 = function(costs) { // Dynamic Programming
+  if (costs.length === 0 || costs[0].length === 0) {
+    return 0;
+  }
+
+  const houseCount = costs.length;
+  const colorCount = costs[0].length;
+  const buf = Array(houseCount).fill().map(() => (
+    Array(colorCount).fill(Number.POSITIVE_INFINITY)
+  ));
+
+  for (let c = 0; c < colorCount; ++c) {
+    buf[0][c] = costs[0][c];
+  }
+  for (let h = 1; h < houseCount; ++h) {
+    for (let c = 0; c < colorCount; ++c) {
+      for (let lastColor = 0; lastColor < colorCount; ++lastColor) {
+        if (c !== lastColor) {
+          buf[h][c] = Math.min(buf[h][c], costs[h][c] + buf[h - 1][lastColor]);
+        }
+      }
+    }
+  }
+  return Math.min(...buf[houseCount - 1]);
+};
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
 var minCostII = function(costs) {
 
   let last = {
-    minCost: 0,
-    minCost2: 0,
+    minCost: 0, // min
+    minCost2: 0, // second to min
     color: -1,
   };
 
