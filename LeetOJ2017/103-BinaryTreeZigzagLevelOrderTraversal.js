a65b4f9126fbac69e184d2ebb16d617f9b50a7bc
anglee <ang.lee@gmail.com>
2017-06-22 08:15:21 -0400

    Alternative LeetOJ2017/103-BinaryTreeZigzagLevelOrderTraversal.js

--- LeetOJ2017/103-BinaryTreeZigzagLevelOrderTraversal.js
+++ LeetOJ2017/103-BinaryTreeZigzagLevelOrderTraversal.js
@@ -9,7 +9,7 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function(root) {
+var zigzagLevelOrder0 = function(root) {
   const ret = [];
 
   let stack = [root];
@@ -33,6 +33,45 @@ var zigzagLevelOrder = function(root) {
     stack = nextStack;
     isOddRow = !isOddRow;
   }
+  return ret;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+  const queue = [{ node: root, rowNumber: 0}];
+  const ret = [];
+  let rowBuffer = [];
+  let currentRowNumber = 0;
+  while (queue.length > 0) {
+    const {node, rowNumber} = queue.shift();
+    if (node === null) { continue; }
+    if (rowNumber === currentRowNumber) {
+      if (currentRowNumber & 1) {
+        rowBuffer.unshift(node.val);
+      } else {
+        rowBuffer.push(node.val);
+      }
+
+    } else {
+      ret.push(rowBuffer);
+      rowBuffer = [node.val];
+      currentRowNumber = rowNumber;
+    }
+
+    queue.push(
+      {node: node.left, rowNumber: currentRowNumber + 1},
+      {node: node.right, rowNumber: currentRowNumber + 1}
+    );
+  }
+  if (rowBuffer.length) {
+    ret.push(rowBuffer);
+  }
+
   return ret;
 };
 
