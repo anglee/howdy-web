5cc511558bafe8291044cdbe6664b7af3b8ffb28
anglee <ang.lee@gmail.com>
2017-06-24 10:35:40 -0400

    Alternative LeetOJ2017/242-ValidAnagram.js

--- LeetOJ2017/242-ValidAnagram.js
+++ LeetOJ2017/242-ValidAnagram.js
@@ -3,7 +3,7 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram0 = function(s, t) {
   if (s.length != t.length) { return false; }
   const counts = {};
   for (let i = 0; i < s.length; ++i) {
@@ -14,6 +14,26 @@ var isAnagram = function(s, t) {
     if (counts[key] !== 0) {
       return false;
     }
+  }
+  return true;
+};
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length != t.length) { return false; }
+  const counts = s.split('').reduce((map, char) => (
+    map.set(char, (map.get(char) || 0) + 1)
+  ), new Map());
+
+  for (let char of t.split('')) {
+    if (!counts.has(char) || counts.get(char) === 0) {
+      return false;
+    }
+    counts.set(char, counts.get(char) - 1);
   }
   return true;
 };
