2d4a3f237c7dfa83bd281c6f5ffa26265b498021
anglee <ang.lee@gmail.com>
2017-06-26 07:05:39 -0400

    Redo LeetOJ2017/314-BinaryTreeVericalOrderTraversal.js

--- LeetOJ2017/314-BinaryTreeVericalOrderTraversal.js
+++ LeetOJ2017/314-BinaryTreeVericalOrderTraversal.js
@@ -9,7 +9,7 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var verticalOrder = function(root) {
+var verticalOrder0 = function(root) {
   const queue = [{x: 0, node: root}];
   const map = new Map();
 
@@ -26,6 +26,38 @@ var verticalOrder = function(root) {
     queue.push({ x: x + 1, node: node.right});
   }
   return Array.from(map.keys()).sort((a, b) => a - b).map(key => map.get(key));
+};
+
+
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalOrder = function(root) {
+  const q = [{node: root, x: 0}];
+  const map = new Map();
+  let minX = Number.POSITIVE_INFINITY;
+  let maxX = Number.NEGATIVE_INFINITY;
+  while (q.length > 0) {
+    const {node, x} = q.shift();
+    if (node) {
+      if (!map.has(x)) {
+        map.set(x, []);
+      }
+      map.get(x).push(node.val);
+      minX = Math.min(minX, x);
+      maxX = Math.max(maxX, x);
+      q.push({node: node.left, x: x - 1});
+      q.push({node: node.right, x: x + 1});
+    }
+  }
+  const ret = [];
+  for (let x = minX; x <= maxX; ++x) {
+    ret.push(map.get(x));
+  }
+  return ret;
 };
 
 export default verticalOrder;
