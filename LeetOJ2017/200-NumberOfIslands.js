562415905c6656bbc2b9455f359081e2216dd887
anglee <ang.lee@gmail.com>
2017-07-02 19:40:51 -0400

    Alternative LeetOJ2017/200-NumberOfIslands.js with union-find

--- LeetOJ2017/200-NumberOfIslands.js
+++ LeetOJ2017/200-NumberOfIslands.js
@@ -2,6 +2,60 @@
  * @param {character[][]} grid
  * @return {number}
  */
+var numIslands0 = function(grid) { // union find
+  const height = grid.length;
+  if (height === 0) { return 0; }
+  const width = grid[0].length;
+
+  const isWater = (x, y) => x < 0 || y < 0 || x >= width || y >= height || grid[y][x] === '0';
+  const findRoot = (x, y) => {
+    while (parentMap[y][x].x !== x || parentMap[y][x].y !== y) {
+      x = parentMap[y][x].x;
+      y = parentMap[y][x].y;
+    }
+    return {x, y};
+  };
+
+  const parentMap = Array(height).fill().map(
+    (_,y) => Array(width).fill().map(
+      (_, x) =>isWater(x, y) ? ({x: null, y: null}) : ({x, y})
+    )
+  );
+  for (let y = 0; y < height; ++y) {
+    for (let x = 0; x < width; ++x) {
+      if (!isWater(x, y)) {
+        const rootOfLeft = isWater(x - 1, y) ? null : findRoot(x - 1, y);
+        const rootOfTop = isWater(x, y - 1) ? null : findRoot(x, y - 1);
+        if (rootOfLeft) {
+          parentMap[y][x] = rootOfLeft;
+          if (rootOfTop) {
+            parentMap[rootOfTop.y][rootOfTop.x] = rootOfLeft;
+          }
+        } else if (rootOfTop) {
+          parentMap[y][x] = rootOfTop;
+        }
+      }
+    }
+  }
+  let ret = 0;
+  for (let y = 0; y < height; ++y) {
+    for (let x = 0; x < width; ++x) {
+      const {x: px, y: py} = parentMap[y][x];
+      if (px === x && py === y) {
+        ret++;
+      }
+    }
+  }
+  return ret;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
 var numIslands = function(grid) {
   const height = grid.length;
   if (height === 0) { return 0; }
