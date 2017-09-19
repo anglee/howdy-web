1b9cbb8571867e98fe6c5dfb2d66bd3a4fa9dc46
anglee <ang.lee@gmail.com>
2017-09-19 08:21:33 -0400

    Improve LeetOJ2017/678-ValidParenthesisString.js

--- LeetOJ2017/678-ValidParenthesisString.js
+++ LeetOJ2017/678-ValidParenthesisString.js
@@ -2,8 +2,7 @@
  * @param {string} s
  * @return {boolean}
  */
-
-var checkValidString = function(s) {
+var checkValidString0 = function(s) {
 
   let counts = new Set([0]);
   for (let ch of s) {
@@ -37,6 +36,44 @@ var checkValidString = function(s) {
   }
 
   return counts.has(0);
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+  // key insight: the above checkValidString0 keeps a set of values,
+  // but actually the set must be a single range
+
+  let range = {
+    high: 0,
+    low: 0,
+  };
+  for (let ch of s) {
+    if (ch === '(') {
+      range.high += 1;
+      range.low += 1;
+    }
+    if (ch === '*') {
+      range.high += 1;
+      range.low -= 1;
+    }
+    if (ch === ')') {
+      range.high -= 1;
+      range.low -= 1;
+    }
+
+    if (range.low < 0) {
+      range.low = 0;
+    }
+    if (range.high < 0) {
+      return false
+    }
+  }
+
+  // return true if range includes 0;
+  return range.high >= 0 && range.low <= 0;
 };
 
 export default checkValidString;
