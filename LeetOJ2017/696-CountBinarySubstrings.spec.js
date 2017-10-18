cf9ec1ed801441d675e00ecc46a7143903cc31f4
anglee <ang.lee@gmail.com>
2017-10-18 07:37:29 -0400

    Fix LeetOJ2017/696-CountBinarySubstrings.spec.js

--- LeetOJ2017/696-CountBinarySubstrings.spec.js
+++ LeetOJ2017/696-CountBinarySubstrings.spec.js
@@ -1,15 +1,17 @@
 import { expect } from 'chai';
-import countBinarySubstrings from './howdy';
+import countBinarySubstrings from './696-CountBinarySubstrings';
 
-describe('countBinarySubstrings', () => {
-  it('should solve the given example #1', () => {
-    const input = '00110011';
-    const expected = 6;
-    expect(countBinarySubstrings(input)).to.equal(expected);
-  });
-  it('should solve the given example #2', () => {
-    const input = '10101';
-    const expected = 4;
-    expect(countBinarySubstrings(input)).to.equal(expected);
+describe('LeetOJ 696-CountBinarySubstrings', () => {
+  describe('countBinarySubstrings', () => {
+    it('should solve the given example #1', () => {
+      const input = '00110011';
+      const expected = 6;
+      expect(countBinarySubstrings(input)).to.equal(expected);
+    });
+    it('should solve the given example #2', () => {
+      const input = '10101';
+      const expected = 4;
+      expect(countBinarySubstrings(input)).to.equal(expected);
+    });
   });
 });
