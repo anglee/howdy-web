b887968b4f2d7ec329c945e1f2a51dd217452b49
anglee <ang.lee@gmail.com>
2017-11-13 22:32:55 -0500

    Alternative LeetOJ2017/721-AccountsMerge.js

--- LeetOJ2017/721-AccountsMerge.js
+++ LeetOJ2017/721-AccountsMerge.js
@@ -10,7 +10,7 @@ class Person {
  * @param {string[][]} accounts
  * @return {string[][]}
  */
-var accountsMerge = function(accounts) {
+var accountsMerge0 = function(accounts) {
   const emailToPersonMap = new Map();
   const peopleSet = new Set();
 
@@ -54,6 +54,66 @@ var accountsMerge = function(accounts) {
       person.name,
       ...Array.from(person.emails).sort()
     ]);
+  });
+  return ret;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+
+  const graph = new Map();
+  const addEdge = (graph, email1, email2) => {
+    if (!graph.has(email1)) {
+      graph.set(email1, new Set());
+    }
+    graph.get(email1).add(email2);
+    if (!graph.has(email2)) {
+      graph.set(email2, new Set());
+    }
+    graph.get(email2).add(email1);
+  };
+
+  const emailToNameMap = new Map();
+  accounts.forEach(([name, ...emails]) => {
+    emails.forEach(email => {
+      emailToNameMap.set(email, name);
+    });
+    const [first, ...rest] = emails;
+    rest.forEach(email => {
+      addEdge(graph, first, email);
+    });
+  });
+
+  const visited = new Set();
+  const dfs = (rootEmail) => {
+    const emails = new Set();
+    const stack = [rootEmail];
+    while (stack.length) {
+      const email = stack.pop();
+      visited.add(email);
+      emails.add(email);
+      if (graph.has(email)) {
+        graph.get(email).forEach(neighbor => {
+          visited.has(neighbor) || stack.push(neighbor);
+        });
+      }
+    }
+    return Array.from(emails);
+  };
+
+  const ret = [];
+  Array.from(emailToNameMap.keys()).forEach(email => {
+    if (!visited.has(email)) {
+      ret.push([
+        emailToNameMap.get(email),
+        ...dfs(email).sort()
+      ]);
+    }
   });
   return ret;
 };
