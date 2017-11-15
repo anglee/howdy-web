e977986cba953363a1f04d70e857bff81beacbec
anglee <ang.lee@gmail.com>
2017-11-14 22:02:40 -0500

    Redo LeetOJ2017/200-NumberOfIslands.js

--- LeetOJ2017/200-NumberOfIslands.js
+++ LeetOJ2017/200-NumberOfIslands.js
@@ -53,6 +53,44 @@ var numIslands0 = function(grid) { // union find
 
 //--------------------------------------------------------------------------------------------------
 
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands1 = function(grid) {
+  const h = grid.length;
+  if (h === 0) { return 0; }
+  const w = grid[0].length;
+  if (w === 0) { return 0; }
+
+  const visited = Array(h).fill().map(() => Array(w).fill(false));
+  const dfs = (y, x) => {
+    if (y < 0 || y >= h || x < 0 || x >= w || grid[y][x] === '0' || visited[y][x]) {
+      return;
+    }
+    visited[y][x] = true;
+    dfs(y - 1, x);
+    dfs(y + 1, x);
+    dfs(y, x + 1);
+    dfs(y, x - 1);
+  };
+
+  let count = 0;
+  for (let y = 0; y < h; ++y) {
+    for (let x = 0; x < w; ++x) {
+      if (!visited[y][x] && grid[y][x] === '1') {
+        dfs(y, x);
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+// export default numIslands1;
+
+//--------------------------------------------------------------------------------------------------
+
 
 /**
  * @param {character[][]} grid
