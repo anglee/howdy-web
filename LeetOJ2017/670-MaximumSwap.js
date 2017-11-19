eaf93fa61e92f28b20a3604789119516b7cfea72
anglee <ang.lee@gmail.com>
2017-11-19 12:54:13 -0500

    LeetOJ2017/670-MaximumSwap.js

--- LeetOJ2017/670-MaximumSwap.js
+++ LeetOJ2017/670-MaximumSwap.js
@@ -9,7 +9,7 @@ const swap = (A, i, j) => {
  * @param {number} num
  * @return {number}
  */
-var maximumSwap = function(num) {
+var maximumSwap0 = function(num) { // O(n ^ 2)
   const getIndexOfMaxOnTheRight = (A, index) => {
     let max = -1;
     let indexOfMax = -1;
@@ -34,12 +34,14 @@ var maximumSwap = function(num) {
   return parseInt(digits.join(''));
 };
 
+//--------------------------------------------------------------------------------------------------
+
 
 /**
  * @param {number} num
  * @return {number}
  */
-var maximumSwap1 = function(num) {
+var maximumSwap1 = function(num) { // O(n)
   const string = `${num}`;
   const digits = string.split('').map(char => parseInt(char));
 
@@ -67,4 +69,60 @@ var maximumSwap1 = function(num) {
   return parseInt(digits.join(''));
 };
 
-export default maximumSwap1;
+//--------------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap2 = function(num) { // O(n ^ 2), Redo maximumSwap0
+  const digits = `${num}`.split('').map(it => parseInt(it));
+  for (let i = 0; i < digits.length - 1; ++i) {
+    let max = digits[digits.length - 1];
+    let maxJ = digits.length - 1;
+    for (let j = digits.length - 2; j >= i + 1; --j) {
+      if (digits[j] > max) {
+        max = digits[j];
+        maxJ = j;
+      }
+    }
+    if (max > digits[i]) {
+      swap(digits, i, maxJ);
+      return parseInt(digits.join(''));
+    }
+  }
+  return num;
+};
+
+//--------------------------------------------------------------------------------------------------
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function(num) {  // O(n), Redo maximumSwap1
+  const digits = `${num}`.split('').map(it => parseInt(it));
+  const maxToTheRight = Array(digits.length).fill(null);
+  const maxToTheRightI = Array(digits.length).fill(null);
+  let max = digits[digits.length - 1];
+  let maxI = digits.length - 1;
+  for (let i = digits.length - 2; i >= 0; --i) {
+    if (digits[i] > max) {
+      max = Math.max(digits[i], max);
+      maxI = i;
+    }
+    maxToTheRight[i] = max;
+    maxToTheRightI[i] = maxI;
+  }
+  for (let i = 0; i < digits.length - 1; ++i) {
+    if (maxToTheRight[i + 1] > digits[i]) {
+      swap(digits, i, maxToTheRightI[i + 1]);
+      return parseInt(digits.join(''));
+    }
+  }
+  return num;
+};
+
+export default maximumSwap;
