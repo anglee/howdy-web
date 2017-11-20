865b683b9ad9c96d09dd98e2b8e65c8b14527cb8
anglee <ang.lee@gmail.com>
2017-11-19 21:39:13 -0500

    Alternative LeetOJ2017/078-Subsets.js

--- LeetOJ2017/078-Subsets.js
+++ LeetOJ2017/078-Subsets.js
@@ -2,13 +2,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
+var subsets0 = function(nums) { // recursive
   if (nums.length === 0) { return[[]]; }
   const [head, ...tail] = nums;
   return subsets(tail).reduce((agg, it) => {
     agg.push(it, [head, ...it]);
     return agg;
   }, []);
+};
+
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) { // DP
+  let buffer = [[]];
+  for (let i = 0; i < nums.length; ++i) {
+    buffer = buffer.reduce((newBuffer, it) => {
+      newBuffer.push(it, it.concat([nums[i]]));
+      return newBuffer;
+    }, []);
+  }
+  return buffer;
 };
 
 export default subsets;
